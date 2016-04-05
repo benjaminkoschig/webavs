154927e8ed798b443710036ad06f75c538826d56
lga <lga@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-04-05 12:08:06 +0000

    Cleaning des scripts (oubli suite 1.15.2)

--- webavs-sql/src/main/sql/cygnus.sql
+++
@@ -1,3 +0,0 @@
---- tous les clients, D0166 gestion du délai de 15 mois
-ALTER TABLE SCHEMA.RFDEMAN ADD EDBRET CHAR(1) DEFAULT '0' NOT NULL;
-reorg table SCHEMA.RFDEMAN;
--- webavs-sql/src/main/sql/framework.sql
+++
@@ -1,2 +0,0 @@
--- Ajout d'une nouvelle propriété pour l'activation des communes politiques
-INSERT INTO SCHEMA.jadeprop VALUES('common.liste.ajouter.commune.politique', 'false');
