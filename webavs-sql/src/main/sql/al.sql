ef69a7b06c7368df0048d6ffcfaa9d53494c537c
lga <lga@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-04-18 15:52:21 +0000

    correction nom caisse

--- webavs-sql/src/main/sql/al.sql
+++ webavs-sql/src/main/sql/al.sql
@@ -1,5 +1,5 @@
 -- ajout d'un nouveau champ au niveau du dossier 
-ALTER TABLE ccvdqua.ALDOS ADD IDGEST varchar(64) DEFAULT null;
+ALTER TABLE SCHEMA.ALDOS ADD IDGEST varchar(64) DEFAULT null;
 reorg table SCHEMA.ALDOS allow read access;
 
 -- ajout d'une nouvelle propeties pour activer la génération de décisions en file d'attente
