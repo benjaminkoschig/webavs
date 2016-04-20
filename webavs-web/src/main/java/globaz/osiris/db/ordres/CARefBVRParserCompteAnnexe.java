778036ea7f9642af12363069360e5b43b99e250d
sco <sco@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-04-20 08:59:56 +0000

    Report 1.15.2-2 - K160413_001 - Référence BVR – La référence BVR générée n'est pas correct. Le numéro d'affilié est tronqué.

---
+++ webavs-web/src/main/java/globaz/osiris/db/ordres/CARefBVRParserCompteAnnexe.java
@@ -0,0 +1,24 @@
+package globaz.osiris.db.ordres;
+
+public class CARefBVRParserCompteAnnexe {
+
+    public String idCompteAnnexe;
+
+    public String getIdCompteAnnexe() {
+        return idCompteAnnexe;
+    }
+
+    public void setIdCompteAnnexe(String idCompteAnnexe) {
+        this.idCompteAnnexe = idCompteAnnexe;
+    }
+
+    public String getIdSection() {
+        return idSection;
+    }
+
+    public void setIdSection(String idSection) {
+        this.idSection = idSection;
+    }
+
+    public String idSection;
+}
