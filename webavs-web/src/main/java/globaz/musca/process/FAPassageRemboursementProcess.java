a80db48f1befa9b5609cc1268286027b330b6e40
sco <sco@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-04-28 12:11:30 +0000

    Report branche 1.15.2-3 - Mode de remboursement.

--- webavs-web/src/main/java/globaz/musca/process/FAPassageRemboursementProcess.java
+++ webavs-web/src/main/java/globaz/musca/process/FAPassageRemboursementProcess.java
@@ -194,7 +194,6 @@ public class FAPassageRemboursementProcess extends FAGenericProcess {
                 }
             }
         }
-
     }
 
     protected String giveDomaineForAdressePaiement() {
