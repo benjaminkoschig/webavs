920854c5c2f4ce7893ce30dc276c5d7450d3a24b
sco <sco@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-06-22 09:20:27 +0000

    WEBAVS-2585 - Attestation fiscale pour indépendant - Faute d'orthographe en Allemand

--- webavs-web/src/main/java/globaz/naos/itext/AFAttestationPersonnelle_doc.java
+++ webavs-web/src/main/java/globaz/naos/itext/AFAttestationPersonnelle_doc.java
@@ -17,6 +17,7 @@ import globaz.globall.util.JANumberFormatter;
 import globaz.hercule.service.CETiersService;
 import globaz.jade.client.util.JadeStringUtil;
 import globaz.jade.common.JadeCodingUtil;
+import globaz.jade.log.JadeLogger;
 import globaz.naos.application.AFApplication;
 import globaz.naos.translation.CodeSystem;
 import globaz.naos.util.AFIDEUtil;
@@ -25,7 +26,6 @@ import globaz.pyxis.api.ITIRole;
 import globaz.pyxis.constantes.IConstantes;
 import globaz.pyxis.db.tiers.TITiersViewBean;
 import globaz.webavs.common.ICommonConstantes;
-import sun.misc.FloatingDecimal;
 
 /**
  * @author SCO
@@ -41,7 +41,7 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
     private static final String TITRE_ATTESTATION_PERSONNELLE = "TITRE_ATTESTATION_PERSONNELLE";
 
     private String annee = null;
-    public ICTDocument catalogue = null;
+    private ICTDocument catalogue = null;
     private String dateEnvoi = null;
     protected boolean hasNext = true;
     private String idTiers = null;
@@ -52,17 +52,29 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
     private String sommeAnterieur = null;
     private String totalCoti = null;
 
+    /**
+     * Constructeur public de la classe
+     * 
+     * @param session
+     * @throws FWIException
+     */
     public AFAttestationPersonnelle_doc() throws Exception {
         this(new BSession(AFApplication.DEFAULT_APPLICATION_NAOS));
     }
 
-    public AFAttestationPersonnelle_doc(BSession session) throws Exception {
+    /**
+     * Constructeur public de la classe en passant une session.
+     * 
+     * @param session
+     * @throws FWIException
+     */
+    public AFAttestationPersonnelle_doc(BSession session) throws FWIException {
         super(session, AFApplication.DEFAULT_APPLICATION_NAOS_REP, session
                 .getLabel(AFAttestationPersonnelle_doc.NOMDOC_ATTESTATION_PERSONNELLE));
     }
 
-    public String _getDateImpression(TITiersViewBean tiers) {
-        String dateImpression = null;
+    private String resolveDateImpression(TITiersViewBean tiers) {
+        String dateImpression;
 
         if (!JadeStringUtil.isEmpty(getDateEnvoi())) {
             dateImpression = JACalendar.format(getDateEnvoi(), tiers.getLangueIso());
@@ -73,11 +85,7 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
         return dateImpression;
     }
 
-    public void _setHeader(CaisseHeaderReportBean bean, TITiersViewBean tiers) throws Exception {
-        if (tiers == null) {
-            tiers = AFUtil.retrieveTiersViewBean(getSession(), getIdTiers());
-        }
-
+    private void setHeader(CaisseHeaderReportBean bean, TITiersViewBean tiers) throws Exception {
         bean.setAdresse(tiers.getAdresseAsString(IConstantes.CS_AVOIR_ADRESSE_COURRIER,
                 ICommonConstantes.CS_APPLICATION_COTISATION));
         bean.setDate(bean.getDate());
@@ -93,7 +101,7 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
 
     @Override
     public void beforeBuildReport() throws FWIException {
-
+        // Not implemented
     }
 
     @Override
@@ -129,18 +137,25 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
         // {3} = somme des années antérieures
         // {4} = total des cotisations
         // {5} = label "FACTUREES" ou "CREDITEES" suivant si total est positif ou pas
+        // {6} = Montant total label "FACTUREES" ou "CREDITEES" suivant si total est positif ou pas
 
-        String valueLabelFactureCredit = null;
-        if (FloatingDecimal.readJavaFormatString(getTotalCoti()).doubleValue() > 0) {
+        String valueLabelFactureCredit;
+        String valueLabelMontantTotalFactureCredit;
+        if (Double.parseDouble(JANumberFormatter.deQuote(getTotalCoti())) > 0) {
             valueLabelFactureCredit = getSession().getApplication().getLabel("FACTUREES", getLangueIsoRequerant());
+            valueLabelMontantTotalFactureCredit = getSession().getApplication().getLabel("MONTANT_TOTAL_FACTUREES",
+                    getLangueIsoRequerant());
         } else {
             valueLabelFactureCredit = getSession().getApplication().getLabel("CREDITEES", getLangueIsoRequerant());
+            valueLabelMontantTotalFactureCredit = getSession().getApplication().getLabel("MONTANT_TOTAL_CREDITEES",
+                    getLangueIsoRequerant());
         }
 
         String[] listeArgument = { formulePolitesse, getAnnee(),
                 JANumberFormatter.fmt(getSommeAnnee(), true, true, false, 2),
                 JANumberFormatter.fmt(getSommeAnterieur(), true, true, false, 2),
-                JANumberFormatter.fmt(getTotalCoti(), true, true, false, 2), valueLabelFactureCredit };
+                JANumberFormatter.fmt(getTotalCoti(), true, true, false, 2), valueLabelFactureCredit,
+                valueLabelMontantTotalFactureCredit };
 
         // Mise en place du texte
         this.setParametres("P_TEXTE1", AFDocumentItextService.getTexte(catalogue, 1, listeArgument));
@@ -157,10 +172,10 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
         fillDocInfo();
 
         // Mise en place du header et du footer
-        setHeaderAndFooter(tiers, _getDateImpression(tiers));
+        setHeaderAndFooter(tiers, resolveDateImpression(tiers));
     }
 
-    public void fillDocInfo() throws Exception {
+    private void fillDocInfo() throws Exception {
         // Numero affilié passé au docinfo
         getDocumentInfo().setDocumentProperty("numero.affilie.formatte", getNumAffilie());
         getDocumentInfo().setDocumentProperty("numero.affilie.non.formatte",
@@ -222,32 +237,33 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
 
     /**
      * Chargement du catalogue
+     * 
+     * @param typeDocument Le type du document
      */
     public void loadCatalogue(String typeDocument) {
-        // récupération du catalogue de texte
-        try {
-            catalogue = AFDocumentItextService.retrieveCatalogue(getSession(), langueIsoRequerant, CodeSystem.DOMAINE,
-                    typeDocument);
-        } catch (Exception e) {
-            getMemoryLog().logMessage(getSession().getLabel("CATALOGUE_INTROUVABLE"), FWMessage.ERREUR,
-                    this.getClass().getName());
-            abort();
-        }
+        loadCatalogue(typeDocument, null, null);
     }
 
     /**
-     * Chargement du catalogue avec un idDocuement
+     * Chargement du catalogue avec un idDocument
+     * 
+     * @param typeDocument
+     * @param idDocument
+     * @param idDocumentDefault
      */
     public void loadCatalogue(String typeDocument, String idDocument, String idDocumentDefault) {
         // récupération du catalogue de texte
+
         try {
             catalogue = AFDocumentItextService.retrieveCatalogue(getSession(), langueIsoRequerant, CodeSystem.DOMAINE,
                     typeDocument, idDocument, idDocumentDefault);
-        } catch (Exception e) {
+        } catch (FWIException e) {
             getMemoryLog().logMessage(getSession().getLabel("CATALOGUE_INTROUVABLE"), FWMessage.ERREUR,
                     this.getClass().getName());
+            JadeLogger.error(this, e);
             abort();
         }
+
     }
 
     @Override
@@ -269,7 +285,14 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
         this.dateEnvoi = dateEnvoi;
     }
 
-    public void setHeaderAndFooter(TITiersViewBean tiers, String dateImpression) throws Exception {
+    /**
+     * Permet de setter le header et le pied de page du document.
+     * 
+     * @param tiers Le tiers reletif au document
+     * @param dateImpression La date d'impression
+     * @throws Exception
+     */
+    private void setHeaderAndFooter(TITiersViewBean tiers, String dateImpression) throws Exception {
 
         // mise en place du header
         ICaisseReportHelper caisseReportHelper = CaisseHelperFactory.getInstance().getCaisseReportHelper(
@@ -277,7 +300,7 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
         CaisseHeaderReportBean headerBean = new CaisseHeaderReportBean();
         headerBean.setDate(dateImpression);
 
-        _setHeader(headerBean, tiers);
+        setHeader(headerBean, tiers);
         caisseReportHelper.addHeaderParameters(this, headerBean);
 
         // mise en place de la signature
@@ -296,7 +319,7 @@ public class AFAttestationPersonnelle_doc extends FWIDocumentManager {
         String serviceSignature = getTemplateProperty(getDocumentInfo(), ACaisseReportHelper.JASP_PROP_SIGN_NOM_SERVICE
                 + getLangueIsoRequerant().toUpperCase());
         // la méthode _replaceVars permet de remplacer les chaine de type
-        // {user.service}
+
         String serviceSignatureFinal = ACaisseReportHelper._replaceVars(serviceSignature, getSession().getUserId(),
                 null);
         signBean.setService(serviceSignatureFinal);
