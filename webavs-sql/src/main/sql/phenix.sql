28d5acde9b20374ae3b5664f3b862ef3963ec88c
mmo <mmo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-09-29 08:53:30 +0000

    Rollback InfoRom 472

--- webavs-sql/src/main/sql/phenix.sql
+++ webavs-sql/src/main/sql/phenix.sql
@@ -1,7 +1,2 @@
---InfoRom472 / WEBAVS-2731
-delete from SCHEMA.JADEPROP where PROPNAME in('phenix.wirr.webservice.uri.wsdl','phenix.wirr.webservice.namespace','phenix.wirr.webservice.name','phenix.wirr.webservice.sedex.sender.id');
-insert into SCHEMA.JADEPROP (propname,propval) values ('phenix.wirr.webservice.uri.wsdl','https://webservice.nrr.zas.admin.ch/wirr_cc/NRRQueryService?wsdl'); 
-insert into SCHEMA.JADEPROP (propname,propval) values ('phenix.wirr.webservice.namespace','http://ws.admin.ch/zas/regcent/NRR/0'); 
-insert into SCHEMA.JADEPROP (propname,propval) values ('phenix.wirr.webservice.name','NRRQueryService'); 
-insert into SCHEMA.JADEPROP (propname,propval) values ('phenix.wirr.webservice.sedex.sender.id',''); 
+
 
