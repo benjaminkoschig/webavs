e7e77c8dc90b1e0ba39749a7ebf1b9d7fdc93046
dma <dma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-11-07 09:25:32 +0000

    Correction pour que les tests passent

--- webavs-web/src/test/java/ch/globaz/common/sql/QueryWriterExecutorTest.java
+++ webavs-web/src/test/java/ch/globaz/common/sql/QueryWriterExecutorTest.java
@@ -1,18 +1,21 @@
 package ch.globaz.common.sql;
 
+import static org.fest.assertions.api.Assertions.*;
+import org.junit.Test;
+import ch.globaz.common.business.exceptions.CommonTechnicalException;
 
 public class QueryWriterExecutorTest {
-    //
-    // @Test(expected = CommonTechnicalException.class)
-    // public void testExecuteAggregateEmptyQuery() throws Exception {
-    // QueryWriterExecutor.query("").forClass(Object.class).execute();
-    // failBecauseExceptionWasNotThrown(CommonTechnicalException.class);
-    // }
-    //
-    // @Test(expected = CommonTechnicalException.class)
-    // public void testExecuteAggregate() throws Exception {
-    // QueryWriterExecutor.select("").execute();
-    // failBecauseExceptionWasNotThrown(CommonTechnicalException.class);
-    // }
+
+    @Test(expected = CommonTechnicalException.class)
+    public void testExecuteAggregateEmptyQuery() throws Exception {
+        QueryWriterExecutor.query("").forClass(Object.class).execute();
+        failBecauseExceptionWasNotThrown(CommonTechnicalException.class);
+    }
+
+    @Test(expected = CommonTechnicalException.class)
+    public void testExecuteAggregate() throws Exception {
+        QueryWriterExecutor.select("").execute();
+        failBecauseExceptionWasNotThrown(CommonTechnicalException.class);
+    }
 
 }
