fa42f2019d0c7aa8495768579043e73868e8ac3f
dcl <dcl@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-12-05 10:37:46 +0000

    Corrections WEBAVS-3459 (K150612_001)

--- webavs-web/src/main/java/globaz/leo/process/handler/LEGenererEtapeHandler.java
+++ webavs-web/src/main/java/globaz/leo/process/handler/LEGenererEtapeHandler.java
@@ -9,6 +9,7 @@ package globaz.leo.process.handler;
 import globaz.globall.db.BProcess;
 import globaz.globall.db.BSession;
 import globaz.globall.db.BTransaction;
+import globaz.globall.util.JACalendar;
 import globaz.jade.client.util.JadeStringUtil;
 import globaz.leo.db.data.LEEnvoiDataSource;
 import globaz.leo.process.generation.ILEGeneration;
@@ -39,7 +40,7 @@ public class LEGenererEtapeHandler {
         ILEGeneration generateur = docHandler.chargeGenerateurDocument(envoiDS, session);
 
         if (generateur != null) {
-            // generateur.setDateImpression(JACalendar.todayJJsMMsAAAA());
+            generateur.setDateImpression(JACalendar.todayJJsMMsAAAA());
             if (parent != null && !JadeStringUtil.isEmpty(parent.getEMailAddress())) {
                 generateur.setEMailAddress(parent.getEMailAddress());
             } else {
