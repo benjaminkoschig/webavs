37f1add546ba9b82ee58fe17ed1d4341d2a2d6e1
lga <lga@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-12-05 09:30:19 +0000

    Vidange des scripts du trunk pour la version 1.18.0

--- webavs-sql/src/main/sql/corvus.sql
+++
@@ -1,7 +0,0 @@
--- D0200 ajout du nouveau code système 489
-delete from SCHEMA.FWCOSP where pcosid = 52810489;
-delete from SCHEMA.FWCOUP where pcosid = 52810489;
-
-insert into SCHEMA.FWCOSP (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise, pspy) values ( 52810489, 'REINFIRMIT', 489 ,1,0,0, 'INFIRMITE_489', 2,1,2,2,2, 2 , 51800010 ,0, 'spy'); 
-insert into SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut, pspy) values ( 52810489, 'F', '489', 'Trisomie 21 (Down-Syndrom)', '20161018000000globlga' ); 
-insert into SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut, pspy) values ( 52810489, 'D', '489', 'Trisomie 21 (Down-Syndrom)', '20161018000000globlga' );
--- webavs-sql/src/main/sql/naos.sql
+++
@@ -1,7 +0,0 @@
---WEBAVS-3170 (S160708_001 AGRIVIT)
-delete from SCHEMA.JADEPROP where PROPNAME in('naos.renouvellement.cap.impression.decisions.pour.type.assurance');
-insert into SCHEMA.JADEPROP (propname,propval) values ('naos.renouvellement.cap.impression.decisions.pour.type.assurance','812028,812030'); 
-
---K161028_001
-update SCHEMA.JADEPROP set propval = '' where propname = 'naos.ide.webservice.uri.wsdl' ;  
-
