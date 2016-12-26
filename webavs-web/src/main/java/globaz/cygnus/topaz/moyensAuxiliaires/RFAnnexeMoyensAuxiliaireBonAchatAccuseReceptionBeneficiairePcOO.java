70d45f166641ff704a865d1dd8a39be0c1869d2a
jwe <jwe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2016-12-26 14:43:46 +0000

    [CCCVS - adresse courrier] - report point 6 - 64 -> http://dev-confluence.ju.globaz.ch/display/TEAM/Corrections+vers+1.17.1-1-CCVS

--- webavs-web/src/main/java/globaz/cygnus/topaz/moyensAuxiliaires/RFAnnexeMoyensAuxiliaireBonAchatAccuseReceptionBeneficiairePcOO.java
+++ webavs-web/src/main/java/globaz/cygnus/topaz/moyensAuxiliaires/RFAnnexeMoyensAuxiliaireBonAchatAccuseReceptionBeneficiairePcOO.java
@@ -57,7 +57,8 @@ public class RFAnnexeMoyensAuxiliaireBonAchatAccuseReceptionBeneficiairePcOO ext
             CaisseHeaderReportBean crBean = new CaisseHeaderReportBean();
 
             // Recherche de l'adresse du tiers
-            String adresse = PRTiersHelper.getAdresseDomicileFormatee(session, idTiers).toString();
+            String adresse = PRTiersHelper.getAdresseCourrierFormatee(session, idTiers, "",
+                    IPRConstantesExternes.TIERS_CS_DOMAINE_APPLICATION_RENTE).toString();
 
             crBean.setAdresse(adresse);
 
