1c4fe2106a17db9788decf50acc8c847c8a1f9b2
cbu <cbu@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-01-30 08:14:14 +0000

    S150318_006 : Contentieux amal (déplacement package)

---
+++ webavs-web/src/main/java/ch/globaz/amal/businessimpl/services/sedexCO/listes/SimpleOutputList_DecomptePaiement_5234_402_1.java
@@ -0,0 +1,70 @@
+package ch.globaz.amal.businessimpl.services.sedexCO.listes;
+
+import ch.globaz.common.domaine.Montant;
+
+public class SimpleOutputList_DecomptePaiement_5234_402_1 {
+    private String nssDebiteur = null;
+    private String nomPrenomDebiteur = null;
+    private Montant paiementDebiteur = null;
+    private Montant rpRetro = null;
+    private Montant annulation = null;
+    private String typeActe = null;
+    private String message = null;
+
+    public String getNssDebiteur() {
+        return nssDebiteur;
+    }
+
+    public void setNssDebiteur(String nssDebiteur) {
+        this.nssDebiteur = nssDebiteur;
+    }
+
+    public String getNomPrenomDebiteur() {
+        return nomPrenomDebiteur;
+    }
+
+    public void setNomPrenomDebiteur(String nomPrenomDebiteur) {
+        this.nomPrenomDebiteur = nomPrenomDebiteur;
+    }
+
+    public Montant getPaiementDebiteur() {
+        return paiementDebiteur;
+    }
+
+    public void setPaiementDebiteur(Montant paiementDebiteur) {
+        this.paiementDebiteur = paiementDebiteur;
+    }
+
+    public Montant getRpRetro() {
+        return rpRetro;
+    }
+
+    public void setRpRetro(Montant rpRetro) {
+        this.rpRetro = rpRetro;
+    }
+
+    public Montant getAnnulation() {
+        return annulation;
+    }
+
+    public void setAnnulation(Montant annulation) {
+        this.annulation = annulation;
+    }
+
+    public String getTypeActe() {
+        return typeActe;
+    }
+
+    public void setTypeActe(String typeActe) {
+        this.typeActe = typeActe;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
