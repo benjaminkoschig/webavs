f32d0b02a9c5e760c6f6941b4a3aa7f87bfcd807
jmc <jmc@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-03-29 08:54:47 +0000

    Mandat P2020

---
+++ webavs-web/src/main/java/globaz/corvus/annonce/service/REAnnonces9eXmlService.java
@@ -0,0 +1,13 @@
+package globaz.corvus.annonce.service;
+
+import globaz.corvus.db.annonces.REAnnoncesAugmentationModification9Eme;
+import ch.admin.zas.rc.RRMeldung9Type;
+
+public class REAnnonces9eXmlService {
+
+    public RRMeldung9Type annonceAugmentationOrdinaire9e(REAnnoncesAugmentationModification9Eme enr01,
+            REAnnoncesAugmentationModification9Eme enr02) {
+        return new RRMeldung9Type();
+    }
+
+}
