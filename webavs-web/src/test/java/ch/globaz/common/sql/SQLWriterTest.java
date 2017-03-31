ed581634477c28b392ed5008ef88239cfd2a44cb
dma <dma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-03-31 09:41:59 +0000

    http://jira.ju.globaz.ch/browse/WEBAVS-3810

--- webavs-web/src/test/java/ch/globaz/common/sql/SQLWriterTest.java
+++ webavs-web/src/test/java/ch/globaz/common/sql/SQLWriterTest.java
@@ -448,7 +448,10 @@ public class SQLWriterTest {
     public void testFields() throws Exception {
         assertThat(SQLWriter.write().fields("test", TableDefTest.class).toSql()).isEqualTo(
                 " TEST.COL1 AS TEST_COL1, TEST.COL2 AS TEST_COL2");
+    }
 
+    @Test
+    public void testFieldsWithSpys() throws Exception {
         assertThat(SQLWriter.write().fields("test", TableDefTestWithSpys.class).toSql()).isEqualTo(
                 " TEST.COL1 AS TEST_COL1, TEST.COL2 AS TEST_COL2, TEST.CSPY AS TEST_CSPY, TEST.PSPY AS TEST_PSPY");
     }
