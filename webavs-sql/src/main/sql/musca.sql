c75ce9ba806f9d4878ba6f2d478602bbea3339bb
est <est@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-04-10 08:52:55 +0000

    modification oubli d'un CCVDQUA

--- webavs-sql/src/main/sql/musca.sql
+++ webavs-sql/src/main/sql/musca.sql
@@ -1,5 +1,5 @@
 -- D0136 - Ajout du module permettant de creer la liste des indépendants AF
-UPDATE SCHEMA.FWINCP SET PINCVA = (SELECT MAX(IDMODFAC)+1 FROM CCVDQUA.FAMODUP) WHERE FWINCP.PINCID = 'FAMODUP';
+UPDATE SCHEMA.FWINCP SET PINCVA = (SELECT MAX(IDMODFAC)+1 FROM SCHEMA.FAMODUP) WHERE PINCID = 'FAMODUP';
 
 INSERT INTO SCHEMA.FAMODUP (IDMODFAC,LIBELLEFR,LIBELLEDE,LIBELLEIT,NOMCLASSE,IDTYPEMODULE,NIVEAUAPPEL,MODIFIERAFACT,PSPY) 
 VALUES ((SELECT MAX(IDMODFAC)+1 FROM SCHEMA.FAMODUP),'Liste des indépendants avec allocations familliales - Revenu minimal non atteint','Liste der Selbstständigerwerbenden mit Familienzulagen - Nicht erreichtes Mindesteinkommen','[IT]Liste des indépendants avec allocations familliales - Revenu minimal non atteint','ch.globaz.al.liste.ALIndeRevenuMinNonAtteintImpl',905002,90,'2','20170101201604globazf');
