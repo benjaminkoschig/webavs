0052f36b7b282d10bc742ca84513a3ed3dfc76b3
cel <cel@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-04-27 09:41:48 +0000

    add doc sur classe (iso20022)

--- webavs-web/src/main/java/globaz/osiris/db/ordres/exception/CAOGRegroupISODepassement.java
+++ webavs-web/src/main/java/globaz/osiris/db/ordres/exception/CAOGRegroupISODepassement.java
@@ -1,5 +1,12 @@
 package globaz.osiris.db.ordres.exception;
 
+/**
+ * Exception typée utilisé par le processus de regroupement des OV à la création des OG (AttacherOrdre) pour remonter le
+ * fait que le dépassement est atteint et déclencher la création de l'OG suivant.
+ * 
+ * @author cel
+ * 
+ */
 public class CAOGRegroupISODepassement extends Exception {
 
     /**
