d1684d7e0d4df9f8493864d248201c77ea889872
sco <sco@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-09-01 07:49:22 +0000

    D0221 - S170403_002 - Modification CCE

--- webavs-sql/src/main/sql/hercule.sql
+++ webavs-sql/src/main/sql/hercule.sql
@@ -23,3 +23,26 @@ INSERT INTO SCHEMA.ENPPFO2 (PFO2ID, PFO1ID, PFO2FO, PFO2VA, PSPY) VALUES ((SELEC
 INSERT INTO SCHEMA.FWCOSP (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) VALUES ( 811014, 'VEGENRECON', 1 ,2,0,0, 'Contrôle non cert. conforme', 2,2,2,2,2, 2 , 10800011 ,0,'20170701Globaz'); 
 INSERT INTO SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut,pspy) VALUES ( 811014, 'D', 'NCC', 'Kontrolle Nicht beglaubigte' ,'20170701Globaz'); 
 INSERT INTO SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut,pspy) VALUES ( 811014, 'F', 'NCC', 'Contrôle non cert. conforme' ,'20170701Globaz'); 
+
+--Rendre inactive les couvertures des affiliés ayant une masse en 50'000 et 150'000
+UPDATE SCHEMA.CECOUVP SET CEBCAV='2', PSPY='20170717133700Globaz' WHERE ceicou in (
+    select CEICOU from (
+        SELECT
+            couv.CEICOU,
+            (select sum(MASSE) from
+                    (SELECT SUM(CUMULMASSE) AS MASSE, compteur.IDRUBRIQUE ,compteur.IDCOMPTEANNEXE, (SELECT propval FROM SCHEMA.JADEPROP where propname = 'pavo.idRubrique') test
+                    FROM SCHEMA.CACPTRP AS compteur
+                    WHERE
+                        ANNEE = 2016
+                    group by compteur.IDRUBRIQUE, compteur.IDCOMPTEANNEXE) AS SOUS
+                    where test like '%'||IDRUBRIQUE||'%' AND CA.IDCOMPTEANNEXE = SOUS.IDCOMPTEANNEXE ) as compteur
+        FROM SCHEMA.CECONTP cont
+            left outer join SCHEMA.afaffip as aff on aff.maiaff = cont.maiaff
+            left outer join SCHEMA.CACPTAP AS CA ON (aff.MALNAF = CA.IDEXTERNEROLE AND CA.IDTIERS = aff.HTITIE  AND CA.IDROLE = 517002)
+            inner join SCHEMA.CECOUVP couv on (couv.MAIAFF = aff.maiaff and couv.CEBCAV = '1')
+            inner join SCHEMA.CEATTPTS att on (att.MALNAF = cont.MALNAF and cont.MDDCDE = att.MPPEDE and cont.MDDCFI = att.MPPEFI and att.CEBAAV = '1')
+        where
+            att.MPNBPT < 11 or att.MPNBPT IS NULL
+        ) 
+    where compteur > 50000 and compteur < 150000
+)
