faccaa4c3a5b4a3ef333be9466e5015794fe4161
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-10-31 12:43:16 +0000

    S170424_022 : Mise en GED - Application sur deux GED

--- webavs-sql/src/main/sql/framework.sql
+++ webavs-sql/src/main/sql/framework.sql
@@ -1,2 +1,4 @@
+DELETE schema.JADEPROP WHERE PROPNAME LIKE 'common.many.ged.actives';
+DELETE schema.JADEPROP WHERE PROPNAME LIKE 'common.many.ged.adapter';
 INSERT INTO schema.JADEPROP (PROPNAME,PROPVAL) VALUES ('common.many.ged.actives','false');
 INSERT INTO schema.JADEPROP (PROPNAME,PROPVAL) VALUES ('common.many.ged.adapter','');
