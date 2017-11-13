e983f33f08aa04a312975a13b8f02ebd333dfe47
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-11-13 13:00:06 +0000

    Mise à jour param

--- webavs-sql/src/main/sql/framework.sql
+++ webavs-sql/src/main/sql/framework.sql
@@ -1,5 +1,3 @@
-DELETE schema.JADEPROP WHERE PROPNAME LIKE 'common.many.ged.actives';
-DELETE schema.JADEPROP WHERE PROPNAME LIKE 'common.many.ged.adapter';
 INSERT INTO schema.JADEPROP (PROPNAME,PROPVAL) VALUES ('common.many.ged.actives','false');
 INSERT INTO schema.JADEPROP (PROPNAME,PROPVAL) VALUES ('common.many.ged.adapter','');
 
