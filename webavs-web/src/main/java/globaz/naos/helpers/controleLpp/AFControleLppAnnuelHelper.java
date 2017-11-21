442ab2b00c914395ab1bdcca01ef2d1cdd9afb01
bjo <bjo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-11-21 09:28:38 +0000

    WEBAVS-4873 : Retour de tests : Contrôle LPP annuel - suppression vérification des droits sur action "naos.taxeCo2.reinjectionListeExcel.afficher"

--- webavs-web/src/main/java/globaz/naos/helpers/controleLpp/AFControleLppAnnuelHelper.java
+++ webavs-web/src/main/java/globaz/naos/helpers/controleLpp/AFControleLppAnnuelHelper.java
@@ -31,27 +31,15 @@ public class AFControleLppAnnuelHelper extends FWHelper {
         AFControleLppAnnuelViewBean vb = (AFControleLppAnnuelViewBean) viewBean;
 
         try {
-            if ((!vb.isModeControleSimulation() && vb.getSession().hasRight(
-                    "naos.taxeCo2.reinjectionListeExcel.afficher", globaz.framework.secure.FWSecureConstants.UPDATE))
-                    || vb.isModeControleSimulation()) {
-                AFControleLppAnnuelProcess process = new AFControleLppAnnuelProcess();
-                process.setSession((BSession) session);
-
-                process.setAnneeDebut(vb.getAnneeDebut());
-                process.setAnneeFin(vb.getAnneeFin());
-
-                process.setTypeAdresse(vb.getTypeAdresse());
-                process.setModeControle(vb.isModeControleSimulation());
-                process.setEMailAddress(vb.getEmail());
-
-                process.setFileName(vb.getFilename());
-
-                BProcessLauncher.start(process);
-            } else {
-                viewBean.setMessage(((BSession) session).getLabel("DROIT_MODIFICATION"));
-                viewBean.setMsgType(FWViewBeanInterface.ERROR);
-            }
-
+            AFControleLppAnnuelProcess process = new AFControleLppAnnuelProcess();
+            process.setSession((BSession) session);
+            process.setAnneeDebut(vb.getAnneeDebut());
+            process.setAnneeFin(vb.getAnneeFin());
+            process.setTypeAdresse(vb.getTypeAdresse());
+            process.setModeControle(vb.isModeControleSimulation());
+            process.setEMailAddress(vb.getEmail());
+            process.setFileName(vb.getFilename());
+            BProcessLauncher.start(process);
         } catch (Exception e) {
             viewBean.setMessage(e.toString());
             viewBean.setMsgType(FWViewBeanInterface.ERROR);
