849216290be7bd6f891a14dcaf5ac7dbfb40e556
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2017-11-30 10:24:26 +0000

    Vidange des scripts

--- webavs-sql/src/main/sql/osiris.sql
+++
@@ -1,9 +0,0 @@
--- S160704_002
-
-insert into SCHEMA.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) values (237592,'OSIREFRUB',1,1,0,0,'PC_PART_CANTONALE_AVS',2, 2,2,2,2,2,10200037,0,'spy');
-insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (237592,'D','','EL AHV Kantonale Anteil','spy');
-insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (237592,'F','','PC AVS part cantonale','spy');
-
-insert into SCHEMA.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) values (237593,'OSIREFRUB',1,1,0,0,'PC_PART_CANTONALE_AI',2, 2,2,2,2,2,10200037,0,'spy');
-insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (237593,'D','','EL IV Kantonale Anteil','spy');
-insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (237593,'F','','PC AI part cantonale','spy');
