1f53aad5f61b23ba0b8e3599a087decfa5640626
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-02-09 14:15:00 +0000

    PCRFM - Ajouter Forcer annulation - dem. refusées

---
+++ webavs-sql/src/main/sql/pegasus.sql
@@ -0,0 +1,7 @@
+insert into SCHEMA.FWCOSP (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise, pspy) values ( 64001010, 'PCETATDEM', 7 ,1,0,0, 'ANNULE', 2,1,2,2,2, 2 , 63000001 ,0, (replace(char(current date), '-', '') concat replace(char(current time), '.', '') concat user) ); 
+insert into SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut, pspy) values ( 64001010, 'F', '', 'Annulé', (replace(char(current date), '-', '') concat replace(char(current time), '.', '') concat user) ); 
+insert into SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut, pspy) values ( 64001010, 'D', '', 'Annulliert', (replace(char(current date), '-', '') concat replace(char(current time), '.', '') concat user) );
+
+insert into SCHEMA.FWCOSP (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise, pspy) values ( 64003007, 'PCETATDRO', 7 ,1,0,0, 'ANNULE', 2,1,2,2,2, 2 , 63000003 ,0, (replace(char(current date), '-', '') concat replace(char(current time), '.', '') concat user) ); 
+insert into SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut, pspy) values ( 64003007, 'F', '', 'Annulé', (replace(char(current date), '-', '') concat replace(char(current time), '.', '') concat user) ); 
+insert into SCHEMA.FWCOUP (pcosid,plaide,pcouid,pcolut, pspy) values ( 64003007, 'D', '', 'Annulliert', (replace(char(current date), '-', '') concat replace(char(current time), '.', '') concat user) ); 
