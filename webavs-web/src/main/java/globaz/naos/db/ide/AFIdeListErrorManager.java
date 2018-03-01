7db3aad7ebe41e81a3815d4ff31a2bddfc5bab21
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-03-01 13:23:35 +0000

    S160502_006 - D0190 - IDE-Développement commun : Gestion des messages d’erreur : la liste des message d erreur ne doit pas contenir les messages des annonces traitées

--- webavs-web/src/main/java/globaz/naos/db/ide/AFIdeListErrorManager.java
+++ webavs-web/src/main/java/globaz/naos/db/ide/AFIdeListErrorManager.java
@@ -2,6 +2,8 @@ package globaz.naos.db.ide;
 
 import globaz.globall.db.BEntity;
 import globaz.globall.db.BManager;
+import globaz.globall.db.BStatement;
+import globaz.naos.translation.CodeSystem;
 import java.io.Serializable;
 
 public class AFIdeListErrorManager extends BManager implements Serializable {
@@ -14,6 +16,15 @@ public class AFIdeListErrorManager extends BManager implements Serializable {
     @Override
     protected BEntity _newEntity() throws Exception {
         return new AFIdeListErrorAnnonce();
+    }
+
+    @Override
+    protected String _getWhere(BStatement statement) {
+        StringBuilder sqlWhere = new StringBuilder();
+        sqlWhere.append(_getCollection() + AFIdeListErrorAnnonce.IDE_ANNONCE_TABLE_NAME + "."
+                + AFIdeListErrorAnnonce.IDE_ANNONCE_FIELD_ETAT + "<>" + CodeSystem.ETAT_ANNONCE_IDE_TRAITE);
+
+        return sqlWhere.toString();
     }
 
 }
