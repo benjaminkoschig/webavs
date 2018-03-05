f3c31d6052d3d851c68bd7587f762e6df978c9ed
yma <yma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-03-05 11:06:59 +0000

    WEBAVS-5082 K180124_002 - Décision de cotisation personnel - Problème montant de cotisation > 999'999

--- webavs-sql/src/main/sql/phenix.sql
+++ webavs-sql/src/main/sql/phenix.sql
@@ -1,2 +1,7 @@
 -- Cette propriete permet d'avoir la case "simule conjoint" automatiquement coche en fonction de l'etat civile (515002 = MARIE)
 INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('phenix.etatCivilCodeSimulConjoint','515002');
+
+-- K180124_002 
+ALTER TABLE CCJUWEB.CPDOENP ALTER COLUMN IDCOT1 SET DATA TYPE decimal(10,2); 
+ALTER TABLE CCJUWEB.CPDOENP ALTER COLUMN IDCOT2 SET DATA TYPE decimal(10,2); 
+CALL SYSPROC.ADMIN_CMD ('REORG TABLE CCJUWEB.CPDOENP');
