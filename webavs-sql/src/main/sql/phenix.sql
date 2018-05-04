6692ddf94d64e3b0388e6eec88d56795c0e4e4e8
bjo <bjo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-05-04 11:17:41 +0000

    Rabat branche portail-fpv sur TRUNK (1.20.0)

--- webavs-sql/src/main/sql/phenix.sql
+++ webavs-sql/src/main/sql/phenix.sql
@@ -5,3 +5,9 @@ INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('phenix.etatCivilCodeSimu
 ALTER TABLE SCHEMA.CPDOENP ALTER COLUMN IDCOT1 SET DATA TYPE decimal(10,2); 
 ALTER TABLE SCHEMA.CPDOENP ALTER COLUMN IDCOT2 SET DATA TYPE decimal(10,2); 
 REORG TABLE SCHEMA.CPDOENP;
+
+-- Ajout ID demande portail dans la table des décisions
+ALTER TABLE SCHEMA.CPDECIP ADD COLUMN EBIDDP NUMERIC(15,0);
+
+-- Insertion libellé libellePassageCotPersAutoPortail dans les propriétés PHENIX
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('phenix.libellePassageCotPersAutoPortail','Décisions personnelles issues du portail');
