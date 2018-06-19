9e447d7b8fedfd0dc988c9a471ebbc2dc854123c
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-06-19 05:54:18 +0000

    FERCIAM - S170815_004 Demande de changement de comportement du mode report Correction du nom de la propriété

--- webavs-sql/src/main/sql/osiris.sql
+++ webavs-sql/src/main/sql/osiris.sql
@@ -1,2 +1,2 @@
 --S170815_004 - Compte auxiliaire - Modification mode report des taxes et amendes - FERCIAM- VON
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('osiris.modeReportAuto.isActivated','false');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('osiris.modeReportAuto','false');
