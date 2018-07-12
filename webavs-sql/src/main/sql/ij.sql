891f0f6f3fd4d44aedc2fbc623f09bb5d17c3899
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-07-12 11:24:12 +0000

    WEBAVS-5574 - S180411_002 - IJAI - Implémentation d'annonces XSD

---
+++ webavs-sql/src/main/sql/ij.sql
@@ -0,0 +1,5 @@
+--Ajout d'une propriété pour choisir le type d'annonce à envoyer 
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('ij.isAnnoncesXML','false');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('ij.centrale.test','true');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('ij.centrale.url','');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('ij.racine.nom.fichier.centrale','');
