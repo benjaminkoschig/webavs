2d993047de71059fd5411fdb36394ca879ca2b34
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-07-24 11:09:22 +0000

    S170908_001 - PC - Adaptation jours d'appoint (spécifique VD-VS) Réadaption du code V3

--- webavs-web/src/main/java/ch/globaz/pegasus/businessimpl/services/models/calcul/joursAppoint/CalculJourAppoint.java
+++ webavs-web/src/main/java/ch/globaz/pegasus/businessimpl/services/models/calcul/joursAppoint/CalculJourAppoint.java
@@ -41,11 +41,16 @@ public class CalculJourAppoint {
  		int nbJoursMois = calEntreeHome.getActualMaximum(Calendar.DAY_OF_MONTH);
 		int nbJours = (nbJoursMois - (calEntreeHome.get(Calendar.DAY_OF_MONTH))) + 1;
 		BigDecimal montantJournalierJoursAppoint;
-		if(montantPcaPrecedant==null && montantPcaCourrant.subtract(montantPcaPrecedant).compareTo(new BigDecimal(0)) == 1){
-		    montantJournalierJoursAppoint = new BigDecimal(0);
+		if(montantPcaPrecedant!=null){
+		    if(montantPcaCourrant.subtract(montantPcaPrecedant).compareTo(new BigDecimal(0)) == 0){
+	            montantJournalierJoursAppoint = new BigDecimal(0);
+	        }else{
+	            montantJournalierJoursAppoint = computeMontantJourAppoint(montantPcaCourrant, montantPcaPrecedant,
+                        nbJoursMois);
+	        }   
 		}else{
-		    montantJournalierJoursAppoint = computeMontantJourAppoint(montantPcaCourrant, montantPcaPrecedant,
-	                nbJoursMois);
+		    montantJournalierJoursAppoint = computeMontantJourAppoint(montantPcaCourrant,   new BigDecimal(0),
+                    nbJoursMois);
 		}
 
 	
