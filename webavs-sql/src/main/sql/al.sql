757b4ea0426302cdc757ae0efd08d6b697b80dbf
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-08-09 07:44:47 +0000

    Modif des commentaires

--- webavs-sql/src/main/sql/al.sql
+++ webavs-sql/src/main/sql/al.sql
@@ -3,8 +3,8 @@ INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('al.generation.annonces.x
 
 -- Ajout de la colonne du pays de domicile de l'enfant dans la table des annonces
 ALTER TABLE SCHEMA.ALANNO ADD COLUMN ARPAYE DECIMAL(3) DEFAULT '0' NOT NULL;
-COMMENT ON COLUMN SCHEMA.ARPAYE IS 'Pays de domicile de l''enfant';
+COMMENT ON COLUMN SCHEMA.ALANNO.ARPAYE IS 'Pays de domicile de l''enfant';
 -- Ajout de la colonne du numéro IDE dans la table des annonces
 ALTER TABLE SCHEMA.ALANNO ADD COLUMN ARNIDE VARCHAR(50);
-COMMENT ON COLUMN ALANNO.ARNIDE IS 'Numéro IDE';
+COMMENT ON COLUMN SCHEMA.ALANNO.ARNIDE IS 'Numéro IDE';
 REORG TABLE SCHEMA.ALANNO;
