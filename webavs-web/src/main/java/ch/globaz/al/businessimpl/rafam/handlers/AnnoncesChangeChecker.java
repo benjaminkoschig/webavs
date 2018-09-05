501e66649cb81d0fb90cea1b0a40e418eefca3b6
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-09-05 09:17:21 +0000

    INFOROM - S180405_006 RAFam : Nouvelle version des annonces au registre -> Création d'une annonce si le pays a été modifié et uniquement pour la nouvelle verison

--- webavs-web/src/main/java/ch/globaz/al/businessimpl/rafam/handlers/AnnoncesChangeChecker.java
+++ webavs-web/src/main/java/ch/globaz/al/businessimpl/rafam/handlers/AnnoncesChangeChecker.java
@@ -1,8 +1,7 @@
 package ch.globaz.al.businessimpl.rafam.handlers;
 
-import globaz.jade.exception.JadeApplicationException;
-import globaz.jade.exception.JadePersistenceException;
 import ch.globaz.al.business.constantes.ALCSDossier;
+import ch.globaz.al.business.constantes.ALConstRafam;
 import ch.globaz.al.business.constantes.enumerations.RafamFamilyAllowanceType;
 import ch.globaz.al.business.constantes.enumerations.RafamFamilyStatus;
 import ch.globaz.al.business.constantes.enumerations.RafamLegalBasis;
@@ -13,17 +12,20 @@ import ch.globaz.al.business.models.droit.DroitComplexModel;
 import ch.globaz.al.business.models.rafam.AnnonceRafamModel;
 import ch.globaz.al.business.services.rafam.InitAnnoncesRafamService;
 import ch.globaz.al.businessimpl.services.ALImplServiceLocator;
+import globaz.jade.exception.JadeApplicationException;
+import globaz.jade.exception.JadePersistenceException;
+import globaz.jade.properties.JadePropertiesService;
 
 public class AnnoncesChangeChecker {
 
     /**
      * Vérifie s'il y a eu des modifications depuis l'envoi de la dernière annonce.
-     * 
+     *
      * @param annonce
      *            Nouvelle annonce correspondant à l'état actuel du droit/dossier
-     * 
+     *
      * @return <code>true</code> si des modifications ont été identifiées
-     * 
+     *
      * @throws JadeApplicationException
      *             Exception levée par la couche métier lorsqu'elle n'a pu effectuer l'opération souhaitée
      * @throws JadePersistenceException
@@ -31,8 +33,8 @@ public class AnnoncesChangeChecker {
      *             faire
      */
     public static boolean hasChanged(AnnonceRafamModel annonce, AnnonceRafamModel lastAnnonce,
-            DossierComplexModel dossier, DroitComplexModel droit) throws JadeApplicationException,
-            JadePersistenceException {
+            DossierComplexModel dossier, DroitComplexModel droit)
+            throws JadeApplicationException, JadePersistenceException {
 
         if (lastAnnonce.isNew()) {
             return false;
@@ -77,9 +79,18 @@ public class AnnoncesChangeChecker {
         if (officeHasChanged(annonce, lastAnnonce)) {
             return true;
         }
+        // Vérification du pays de résidence de l'enfant seulement avec la nouvelle version des schéma xsd
+        if ("true".equals(JadePropertiesService.getInstance().getProperty(ALConstRafam.VERSION_ANNONCES_XSD_4_1))
+                && paysResidenceEnfantChanged(annonce, lastAnnonce)) {
+            return true;
+        }
 
         return false;
 
+    }
+
+    private static boolean paysResidenceEnfantChanged(AnnonceRafamModel annonce, AnnonceRafamModel lastAnnonce) {
+        return !lastAnnonce.getCodeCentralePaysEnfant().equals(annonce.getCodeCentralePaysEnfant());
     }
 
     private static boolean nssEnfantHasChanged(AnnonceRafamModel annonce, AnnonceRafamModel lastAnnonce) {
@@ -98,20 +109,20 @@ public class AnnoncesChangeChecker {
 
     private static boolean statutFamilialHasChanged(AnnonceRafamModel lastAnnonce, DroitComplexModel droit)
             throws JadeApplicationException {
-        return !RafamFamilyStatus.getFamilyStatus(lastAnnonce.getCodeStatutFamilial()).equals(
-                RafamFamilyStatus.getFamilyStatusCS(droit.getDroitModel().getStatutFamilial()));
+        return !RafamFamilyStatus.getFamilyStatus(lastAnnonce.getCodeStatutFamilial())
+                .equals(RafamFamilyStatus.getFamilyStatusCS(droit.getDroitModel().getStatutFamilial()));
     }
 
     private static boolean echeanceDroitHasChanged(AnnonceRafamModel annonce, AnnonceRafamModel lastAnnonce,
             DroitComplexModel droit) {
-        return !lastAnnonce.getEcheanceDroit().equals(
-                (annonce == null ? droit.getDroitModel().getFinDroitForcee() : annonce.getEcheanceDroit()));
+        return !lastAnnonce.getEcheanceDroit()
+                .equals((annonce == null ? droit.getDroitModel().getFinDroitForcee() : annonce.getEcheanceDroit()));
     }
 
     private static boolean debutDroitHasChanged(AnnonceRafamModel annonce, AnnonceRafamModel lastAnnonce,
             DroitComplexModel droit) {
-        return !lastAnnonce.getDebutDroit().equals(
-                (annonce == null ? droit.getDroitModel().getDebutDroit() : annonce.getDebutDroit()));
+        return !lastAnnonce.getDebutDroit()
+                .equals((annonce == null ? droit.getDroitModel().getDebutDroit() : annonce.getDebutDroit()));
     }
 
     private static boolean baseLegalHasChanged(AnnonceRafamModel lastAnnonce, DossierComplexModel dossier,
@@ -119,8 +130,8 @@ public class AnnoncesChangeChecker {
 
         InitAnnoncesRafamService initAnnonceService = ALImplServiceLocator.getInitAnnoncesRafamService();
 
-        return !RafamLegalBasis.getLegalBasis(lastAnnonce.getBaseLegale()).equals(
-                initAnnonceService.getBaseLegale(dossier, droit));
+        return !RafamLegalBasis.getLegalBasis(lastAnnonce.getBaseLegale())
+                .equals(initAnnonceService.getBaseLegale(dossier, droit));
     }
 
     private static boolean officeHasChanged(AnnonceRafamModel annonce, AnnonceRafamModel lastAnnonce)
@@ -135,9 +146,9 @@ public class AnnoncesChangeChecker {
 
     /**
      * Vérifie si le statut du dossier en cours de traitement à changé depuis l'enregistrement de la dernière annonce.
-     * 
+     *
      * @return <code>true</code> si le statut à changé, <code>false</code> sinon
-     * 
+     *
      * @throws JadeApplicationException
      *             Exception levée par la couche métier lorsqu'elle n'a pu effectuer l'opération souhaitée
      * @throws JadePersistenceException
