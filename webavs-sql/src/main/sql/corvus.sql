6ff97d9d863968a859769e8047d82a31bfd4291b
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-10-18 07:20:37 +0000

    INFOROM - S180502_013 D0230 - Choix du Tribunal pour un éventuel recours MAJ SQL

--- webavs-sql/src/main/sql/corvus.sql
+++ webavs-sql/src/main/sql/corvus.sql
@@ -1,3 +1,3 @@
 --Ajout d'une propriété du type de caisse (cantonale ou professionel) qui permet de sélectionner le bon tribunal pour les décisions dans les rentes
 
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('corvus.TYPE_de_CAISSE','');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL) VALUES ('corvus.TYPE_de_CAISSE','Caisse_PROF');
