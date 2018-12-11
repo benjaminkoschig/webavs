bd81735fb77b676c3b63d4c839bee6ce3069161c
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2018-12-11 08:45:56 +0000

    [S180928_001] Adaptation des règles 400

--- webavs-web/src/main/java/globaz/apg/rapg/rules/Rule400.java
+++ webavs-web/src/main/java/globaz/apg/rapg/rules/Rule400.java
@@ -1,31 +1,25 @@
 package globaz.apg.rapg.rules;
 
-import globaz.apg.api.annonces.IAPAnnonce;
+import java.util.ArrayList;
+import java.util.List;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import globaz.apg.api.droits.IAPDroitLAPG;
-import globaz.apg.api.prestation.IAPPrestation;
-import globaz.apg.db.droits.APDroitAPGJointTiers;
-import globaz.apg.db.droits.APDroitAPGJointTiersManager;
 import globaz.apg.db.droits.APDroitMaterniteJointTiers;
 import globaz.apg.db.droits.APDroitMaterniteJointTiersManager;
-import globaz.apg.db.prestation.APPrestation;
-import globaz.apg.db.prestation.APPrestationManager;
 import globaz.apg.enums.APGenreServiceAPG;
-import globaz.apg.enums.APTypeDePrestation;
 import globaz.apg.exceptions.APRuleExecutionException;
 import globaz.apg.interfaces.APDroitAvecParent;
 import globaz.apg.pojo.APChampsAnnonce;
+import globaz.jade.client.util.JadeDateUtil;
 import globaz.jade.client.util.JadePeriodWrapper;
-import java.util.ArrayList;
-import java.util.List;
-import org.joda.time.LocalDate;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeFormatterBuilder;
 
 /**
- * <strong>Règles de validation des plausibilités RAPG</br> Description :</strong></br> Période de contrôle : 
+ * <strong>Règles de validation des plausibilités RAPG</br>
+ * Description :</strong></br>
+ * Période de contrôle :
  * Si, durant le congé maternité code 90 et codes 10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 30, 40, 41, 50 </br>
- * 
+ *
  * @author mmo
  */
 public class Rule400 extends Rule {
@@ -43,7 +37,7 @@ public class Rule400 extends Rule {
         if (typeAnnonce == 1) {
             validNotEmpty(serviceType, "serviceType");
         }
-        
+
         List<String> services = new ArrayList<String>();
         services.add("10");
         services.add("11");
@@ -67,9 +61,9 @@ public class Rule400 extends Rule {
             String nss = champsAnnonce.getInsurant();
             String startOfPeriod = champsAnnonce.getStartOfPeriod();
             String endOfPeriod = champsAnnonce.getEndOfPeriod();
-            
+
             DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyyMMdd");
-            
+
             testDateNotEmptyAndValid(startOfPeriod, "startOfPeriod");
             testDateNotEmptyAndValid(endOfPeriod, "endOfPeriod");
             validNotEmpty(nss, "nss");
@@ -93,31 +87,43 @@ public class Rule400 extends Rule {
                     manager.find();
                     List<APDroitAvecParent> tousLesDroits = manager.getContainer();
                     droitsSansParents = skipDroitParent(tousLesDroits);
-                    
+
                     for (Object d : tousLesDroits) {
                         APDroitMaterniteJointTiers droit = (APDroitMaterniteJointTiers) d;
-//                        LocalDate debutPeriodDate = LocalDate.parse(droit.getUneDateDebutPeriode(), formatter);
-//                        LocalDate finPeriodDate = LocalDate.parse(droit.getUneDateFinPeriode(), formatter);
-//                        
-//                        String debutPeriodeString = String.valueOf(debutPeriodDate.getYear()) + String.valueOf(debutPeriodDate.getMonthOfYear()) 
-//                                                    + String.valueOf(debutPeriodDate.getDayOfMonth()); 
-//                        
-//                        String finPeriodeString = String.valueOf(finPeriodDate.getYear()) + String.valueOf(finPeriodDate.getMonthOfYear()) 
-//                                                    + String.valueOf(finPeriodDate.getDayOfMonth());
 
-                        boolean isPrestationDansPeriodeControle = periodeControle.isDateDansLaPeriode(droit.getUneDateDebutPeriode()) 
-                                && periodeControle.isDateDansLaPeriode(droit.getUneDateFinPeriode());
-                        if (isPrestationDansPeriodeControle) {
+                        boolean isPrestationDansPeriodeControle = isHorsPeriode(startOfPeriod, endOfPeriod, droit);
+
+                        // periodeControle.isDateDansLaPeriode(droit.getUneDateDebutPeriode())
+                        // && periodeControle.isDateDansLaPeriode(droit.getUneDateFinPeriode());
+                        if (!isPrestationDansPeriodeControle) {
                             return false;
-                        }                    
-                    }                                       
+                        }
+                    }
                 } catch (Exception e) {
                     throwRuleExecutionException(e);
-                }              
+                }
             } catch (Exception e) {
                 throwRuleExecutionException(e);
             }
         }
         return true;
+    }
+
+    private boolean isHorsPeriode(String startOfPeriod, String endOfPeriod, APDroitMaterniteJointTiers droit) {
+        boolean isHorsPeriode = false;
+        String dateDebutMat = droit.getUneDateDebutPeriode();
+        String dateFinMat = droit.getUneDateFinPeriode();
+        if ((JadeDateUtil.isDateBefore(dateDebutMat, startOfPeriod)
+                && JadeDateUtil.isDateBefore(dateDebutMat, endOfPeriod))
+                && (JadeDateUtil.isDateBefore(dateFinMat, startOfPeriod)
+                        && JadeDateUtil.isDateBefore(dateFinMat, endOfPeriod))
+                || (JadeDateUtil.isDateAfter(dateDebutMat, startOfPeriod)
+                        && JadeDateUtil.isDateAfter(dateDebutMat, endOfPeriod))
+                        && (JadeDateUtil.isDateAfter(dateFinMat, startOfPeriod)
+                                && JadeDateUtil.isDateAfter(dateFinMat, endOfPeriod))) {
+
+        }
+
+        return isHorsPeriode;
     }
 }
