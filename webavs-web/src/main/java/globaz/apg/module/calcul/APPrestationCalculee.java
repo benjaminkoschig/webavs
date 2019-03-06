063cf431990780b8798bb2b5727abb2401706c6f
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2019-03-06 08:55:44 +0000

    FERCIAB - S170313_002 - APG - Ajout des jours isolés APG

--- webavs-web/src/main/java/globaz/apg/module/calcul/APPrestationCalculee.java
+++ webavs-web/src/main/java/globaz/apg/module/calcul/APPrestationCalculee.java
@@ -5,7 +5,7 @@ import globaz.globall.util.JADate;
 
 /**
  * Descpription
- * 
+ *
  * @author scr Date de création 23 mai 05
  */
 public class APPrestationCalculee {
@@ -28,6 +28,7 @@ public class APPrestationCalculee {
     private APResultatCalcul resultatCalcul = null;
     private FWCurrency revenuDeterminantMoyen = null;
     private String typePrestation = null;
+    private String csGenrePrestation = null;
 
     // ~ Constructors
     // ---------------------------------------------------------------------------------------------------
@@ -48,7 +49,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut contenu annonce
-     * 
+     *
      * @return la valeur courante de l'attribut contenu annonce
      */
     public String getContenuAnnonce() {
@@ -60,7 +61,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut date debut
-     * 
+     *
      * @return la valeur courante de l'attribut date debut
      */
     public JADate getDateDebut() {
@@ -69,7 +70,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut date fin
-     * 
+     *
      * @return la valeur courante de l'attribut date fin
      */
     public JADate getDateFin() {
@@ -78,7 +79,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut droit acquis
-     * 
+     *
      * @return la valeur courante de l'attribut droit acquis
      */
     public FWCurrency getDroitAcquis() {
@@ -87,7 +88,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut etat
-     * 
+     *
      * @return la valeur courante de l'attribut etat
      */
     public String getEtat() {
@@ -96,7 +97,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut frais garde
-     * 
+     *
      * @return la valeur courante de l'attribut frais garde
      */
     public FWCurrency getFraisGarde() {
@@ -105,7 +106,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut id droit
-     * 
+     *
      * @return la valeur courante de l'attribut id droit
      */
     public String getIdDroit() {
@@ -114,7 +115,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut montant journalier
-     * 
+     *
      * @return la valeur courante de l'attribut montant journalier
      */
     public FWCurrency getMontantJournalier() {
@@ -123,7 +124,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut nombre jours soldes
-     * 
+     *
      * @return la valeur courante de l'attribut nombre jours soldes
      */
     public String getNombreJoursSoldes() {
@@ -132,7 +133,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut resultat calcul
-     * 
+     *
      * @return la valeur courante de l'attribut resultat calcul
      */
     public APResultatCalcul getResultatCalcul() {
@@ -141,7 +142,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut revenu determinant moyen
-     * 
+     *
      * @return la valeur courante de l'attribut revenu determinant moyen
      */
     public FWCurrency getRevenuDeterminantMoyen() {
@@ -150,7 +151,7 @@ public class APPrestationCalculee {
 
     /**
      * getter pour l'attribut type prestation
-     * 
+     *
      * @return la valeur courante de l'attribut type prestation
      */
     public String getTypePrestation() {
@@ -159,7 +160,7 @@ public class APPrestationCalculee {
 
     /**
      * @param basicDailyAmount
-     *            the basicDailyAmount to set
+     *                             the basicDailyAmount to set
      */
     public void setBasicDailyAmount(FWCurrency basicDailyAmount) {
         this.basicDailyAmount = basicDailyAmount;
@@ -167,9 +168,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut contenu annonce
-     * 
+     *
      * @param string
-     *            une nouvelle valeur pour cet attribut
+     *                   une nouvelle valeur pour cet attribut
      */
     public void setContenuAnnonce(String string) {
         contenuAnnonce = string;
@@ -177,9 +178,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut date debut
-     * 
+     *
      * @param date
-     *            une nouvelle valeur pour cet attribut
+     *                 une nouvelle valeur pour cet attribut
      */
     public void setDateDebut(JADate date) {
         dateDebut = date;
@@ -187,9 +188,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut date fin
-     * 
+     *
      * @param date
-     *            une nouvelle valeur pour cet attribut
+     *                 une nouvelle valeur pour cet attribut
      */
     public void setDateFin(JADate date) {
         dateFin = date;
@@ -197,9 +198,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut droit acquis
-     * 
+     *
      * @param currency
-     *            une nouvelle valeur pour cet attribut
+     *                     une nouvelle valeur pour cet attribut
      */
     public void setDroitAcquis(FWCurrency currency) {
         droitAcquis = currency;
@@ -207,9 +208,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut etat
-     * 
+     *
      * @param s
-     *            une nouvelle valeur pour cet attribut
+     *              une nouvelle valeur pour cet attribut
      */
     public void setEtat(String s) {
         etat = s;
@@ -217,9 +218,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut frais garde
-     * 
+     *
      * @param currency
-     *            une nouvelle valeur pour cet attribut
+     *                     une nouvelle valeur pour cet attribut
      */
     public void setFraisGarde(FWCurrency currency) {
         fraisGarde = currency;
@@ -227,9 +228,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut id droit
-     * 
+     *
      * @param string
-     *            une nouvelle valeur pour cet attribut
+     *                   une nouvelle valeur pour cet attribut
      */
     public void setIdDroit(String string) {
         idDroit = string;
@@ -237,9 +238,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut montant journalier
-     * 
+     *
      * @param currency
-     *            une nouvelle valeur pour cet attribut
+     *                     une nouvelle valeur pour cet attribut
      */
     public void setMontantJournalier(FWCurrency currency) {
         montantJournalier = currency;
@@ -247,9 +248,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut nombre jours soldes
-     * 
+     *
      * @param s
-     *            une nouvelle valeur pour cet attribut
+     *              une nouvelle valeur pour cet attribut
      */
     public void setNombreJoursSoldes(String s) {
         nombreJoursSoldes = s;
@@ -257,9 +258,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut resultat calcul
-     * 
+     *
      * @param calcul
-     *            une nouvelle valeur pour cet attribut
+     *                   une nouvelle valeur pour cet attribut
      */
     public void setResultatCalcul(APResultatCalcul calcul) {
         resultatCalcul = calcul;
@@ -267,9 +268,9 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut revenu determinant moyen
-     * 
+     *
      * @param currency
-     *            une nouvelle valeur pour cet attribut
+     *                     une nouvelle valeur pour cet attribut
      */
     public void setRevenuDeterminantMoyen(FWCurrency currency) {
         revenuDeterminantMoyen = currency;
@@ -277,12 +278,20 @@ public class APPrestationCalculee {
 
     /**
      * setter pour l'attribut type prestation
-     * 
+     *
      * @param s
-     *            une nouvelle valeur pour cet attribut
+     *              une nouvelle valeur pour cet attribut
      */
     public void setTypePrestation(String s) {
         typePrestation = s;
+    }
+
+    public String getCsGenrePrestation() {
+        return csGenrePrestation;
+    }
+
+    public void setCsGenrePrestation(String csGenrePrestation) {
+        this.csGenrePrestation = csGenrePrestation;
     }
 
 }
