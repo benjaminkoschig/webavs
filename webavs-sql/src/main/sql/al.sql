2b1c33a92350e14135d30b429ef63ccdb4c4daa9
eniv <eniv@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2019-04-01 12:37:29 +0000

    [WebAVS-6329] Modification script pour valeur à False par défaut.

--- webavs-sql/src/main/sql/al.sql
+++ webavs-sql/src/main/sql/al.sql
@@ -1,4 +1,4 @@
 INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('al.motifE411','false','20190301120000Globaz    ','20190301120000Globaz    ');
 
 --WEBAVS-6147 - Ajout d'un paramètre pour ajouter un caractère au NSS lors d'édition CSV
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('al.recapFormatNss','true','2019032620000Globaz    ','2019032620000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('al.recapFormatNss','false','2019032620000Globaz    ','2019032620000Globaz    ');
