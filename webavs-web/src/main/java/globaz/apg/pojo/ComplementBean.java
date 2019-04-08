7060f5360e2a58ab547ff434a4eebfd533b1127b
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2019-04-08 08:13:29 +0000

    WEBAVS-6122 - FERCIAB - S170313_002 - APG - Ajout des compléments APG : Comptabilisation : JOURS ISOLES + COMPLEMENTS

---
+++ webavs-web/src/main/java/globaz/apg/pojo/ComplementBean.java
@@ -0,0 +1,98 @@
+package globaz.apg.pojo;
+
+import globaz.osiris.api.APIRubrique;
+
+public class ComplementBean {
+
+    private APIRubrique rubriqueParitaireParticipationCotisation;
+    private APIRubrique rubriqueParitaireMontantBrut;
+    private APIRubrique rubriqueParitaireRestitution;
+    private APIRubrique rubriquePersonnelCotisationAC;
+    private APIRubrique rubriquePersonnelCotisationAVS;
+    private APIRubrique rubriquePersonnelParticipationCotisation;
+    private APIRubrique rubriquePersonnelMontantBrut;
+    private APIRubrique rubriquePersonnelRestitution;
+    private APIRubrique rubriqueParitaireImpotSource;
+    private APIRubrique rubriquePersonnelImpotSource;
+
+
+    public APIRubrique getRubriqueParitaireParticipationCotisation() {
+        return rubriqueParitaireParticipationCotisation;
+    }
+
+    public void setRubriqueParitaireParticipationCotisation(APIRubrique rubriqueParitaireParticipationCotisation) {
+        this.rubriqueParitaireParticipationCotisation = rubriqueParitaireParticipationCotisation;
+    }
+
+    public APIRubrique getRubriqueParitaireMontantBrut() {
+        return rubriqueParitaireMontantBrut;
+    }
+
+    public void setRubriqueParitaireMontantBrut(APIRubrique rubriqueParitaireMontantBrut) {
+        this.rubriqueParitaireMontantBrut = rubriqueParitaireMontantBrut;
+    }
+
+    public APIRubrique getRubriqueParitaireRestitution() {
+        return rubriqueParitaireRestitution;
+    }
+
+    public void setRubriqueParitaireRestitution(APIRubrique rubriqueParitaireRestitution) {
+        this.rubriqueParitaireRestitution = rubriqueParitaireRestitution;
+    }
+
+    public APIRubrique getRubriquePersonnelCotisationAC() {
+        return rubriquePersonnelCotisationAC;
+    }
+
+    public void setRubriquePersonnelCotisationAC(APIRubrique rubriquePersonnelCotisationAC) {
+        this.rubriquePersonnelCotisationAC = rubriquePersonnelCotisationAC;
+    }
+
+    public APIRubrique getRubriquePersonnelCotisationAVS() {
+        return rubriquePersonnelCotisationAVS;
+    }
+
+    public void setRubriquePersonnelCotisationAVS(APIRubrique rubriquePersonnelCotisationAVS) {
+        this.rubriquePersonnelCotisationAVS = rubriquePersonnelCotisationAVS;
+    }
+
+    public APIRubrique getRubriquePersonnelParticipationCotisation() {
+        return rubriquePersonnelParticipationCotisation;
+    }
+
+    public void setRubriquePersonnelParticipationCotisation(APIRubrique rubriquePersonnelParticipationCotisation) {
+        this.rubriquePersonnelParticipationCotisation = rubriquePersonnelParticipationCotisation;
+    }
+
+    public APIRubrique getRubriquePersonnelMontantBrut() {
+        return rubriquePersonnelMontantBrut;
+    }
+
+    public void setRubriquePersonnelMontantBrut(APIRubrique rubriquePersonnelMontantBrut) {
+        this.rubriquePersonnelMontantBrut = rubriquePersonnelMontantBrut;
+    }
+
+    public APIRubrique getRubriquePersonnelRestitution() {
+        return rubriquePersonnelRestitution;
+    }
+
+    public void setRubriquePersonnelRestitution(APIRubrique rubriquePersonnelRestitution) {
+        this.rubriquePersonnelRestitution = rubriquePersonnelRestitution;
+    }
+
+    public APIRubrique getRubriqueParitaireImpotSource() {
+        return rubriqueParitaireImpotSource;
+    }
+
+    public void setRubriqueParitaireImpotSource(APIRubrique rubriqueParitaireImpotSource) {
+        this.rubriqueParitaireImpotSource = rubriqueParitaireImpotSource;
+    }
+
+    public APIRubrique getRubriquePersonnelImpotSource() {
+        return rubriquePersonnelImpotSource;
+    }
+
+    public void setRubriquePersonnelImpotSource(APIRubrique rubriquePersonnelImpotSource) {
+        this.rubriquePersonnelImpotSource = rubriquePersonnelImpotSource;
+    }
+}
