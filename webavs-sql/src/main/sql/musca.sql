b177cf2a26a08e7eb6508aa15ba079ea4421c64c
eniv <eniv@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2019-12-19 15:11:36 +0000

    [WEBAVS-7081] Modification a false du script

--- webavs-sql/src/main/sql/musca.sql
+++ webavs-sql/src/main/sql/musca.sql
@@ -1,2 +1,2 @@
 -- WEBAVS-7173 : Affichage du taux moyen. Ajout d'une propriété
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.afficheTauxParPalier','true','20191216100439globaz    ','20191216100439globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.afficheTauxParPalier','false','20191216100439globaz    ','20191216100439globaz    ');
