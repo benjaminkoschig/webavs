7a9e09e11da20c314713780be8205432e2acd598
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-01-08 09:39:41 +0000

    K200108_001 - RAFAM : Impossible d'effectuer certaines mutations sur dossiers : correction montant IS

--- webavs-web/src/main/java/ch/globaz/al/business/models/droit/CalculBusinessModel.java
+++ webavs-web/src/main/java/ch/globaz/al/business/models/droit/CalculBusinessModel.java
@@ -42,6 +42,11 @@ public class CalculBusinessModel implements Comparable<Object> {
      */
     private String calculResultMontantEffectifCanton = null;
     /**
+     * Montant Impot à la source
+     */
+    private String calculResultMontantIS = null;
+
+    /**
      * Droit lié à la prestation
      */
     private DroitComplexModel droit = null;
@@ -219,6 +224,13 @@ public class CalculBusinessModel implements Comparable<Object> {
      */
     public String getCalculResultMontantEffectifCanton() {
         return calculResultMontantEffectifCanton;
+    }
+
+    /**
+     * @return the calculResultMontantIS
+     */
+    public String getCalculResultMontantIS() {
+        return calculResultMontantIS;
     }
 
     /**
@@ -400,6 +412,14 @@ public class CalculBusinessModel implements Comparable<Object> {
      */
     public void setCalculResultMontantEffectifCanton(String calculResultMontantEffectifCanton) {
         this.calculResultMontantEffectifCanton = calculResultMontantEffectifCanton;
+    }
+
+    /**
+     * @param calculResultMontantIS
+     *            the calculResultMontantIS to set
+     */
+    public void setCalculResultMontantIS(String calculResultMontantIS) {
+        this.calculResultMontantIS = calculResultMontantIS;
     }
 
     /**
