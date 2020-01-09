62106f41098ff1407cb4d374364ed597fee731b4
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-01-09 10:44:18 +0000

    CCVD - S180713_009 RFM - Importation Secutel-AVASAD

---
+++ webavs-sql/src/main/sql/cygnus.sql
@@ -0,0 +1,5 @@
+-- CCVD - S180713_009 RFM - Importation Secutel-AVASAD
+insert into SCHEMA.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) values (66000507,'RFLIBSTS',1,1,0,0,'Secutel',2, 2,2,2,2,2,65000007,0,'spy');
+insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (66000507,'D','SECUTEL','Secutel','spy');
+insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (66000507,'F','SECUTEL','Secutel','spy');
+insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (66000507,'I','SECUTEL','Secutel','spy');
