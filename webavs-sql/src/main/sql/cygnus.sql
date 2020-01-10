206e8f13ee9eac79d55f21b0b6e80c3abe0a3213
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-01-10 10:15:45 +0000

    CCVD - S180713_009 RFM - Importation Secutel-AVASAD

--- webavs-sql/src/main/sql/cygnus.sql
+++ webavs-sql/src/main/sql/cygnus.sql
@@ -1,5 +1,8 @@
--- CCVD - S180713_009 RFM - Importation Secutel-AVASAD
-insert into SCHEMA.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) values (66000507,'RFLIBSTS',1,1,0,0,'Secutel',2, 2,2,2,2,2,65000007,0,'spy');
+-- Specifique CCVD - S180713_009 RFM - Importation Secutel-AVASAD
+-- A deplacer dans version 1.23.?-CCVD ?
+insert into SCHEMA.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) values (66000507,'RFLIBSTS',175,1,0,0,'3_secutel',2, 1,2,2,2,2,65000007,0,'spy');
 insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (66000507,'D','SECUTEL','Secutel','spy');
 insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (66000507,'F','SECUTEL','Secutel','spy');
 insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (66000507,'I','SECUTEL','Secutel','spy');
+
+insert into SCHEMA.RFSTSOI (eeists, eencod, eeitso) values (66000507, '08', 66000052);
