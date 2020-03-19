632fce7facadac0a2a960dd3d61c84ffe006224e
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-03-19 12:17:50 +0000

    POAVS-2816 - INFOROM - S200210_006 APGMat webservice Création des propriétés

---
+++ webavs-sql/src/main/sql/apg.sql
@@ -0,0 +1,11 @@
+-- Ajout propriété pour la webservices des breakrules
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.endpoint.address','https://www.rapg-a.zas.admin.ch/ApgCore/ws/consultation','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.keystore.password','tsz9ty9of+53aZlasRVLjw==','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.keystore.path','/home/wascnt/keys/T6-150000-1_2','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.keystore.type','JKS','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.seodor.wsdl.path','wsdl/rapg/rapg-query.wsdl','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.ssi.context.type','TLS','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.webservice.name','RapgConsultationService-1','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.webservice.namespace','http://www.zas.admin.ch/rapg/webservice/consultation/1','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.rapg.webservice.sedex.sender.id','T6-150000-1','20200318120000Globaz    ','20200318120000Globaz    ');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('apg.rapg.activer.webservice','true','20200318120000Globaz    ','20200318120000Globaz    ');
