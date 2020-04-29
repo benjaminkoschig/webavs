b28c6f0095b02f5e799002dc058243acbac3f833
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-04-29 08:04:38 +0000

    POAVS-2816 - INFOROM - S200210_006 APGMat webservice

--- webavs-web/src/main/java/globaz/apg/helpers/droits/APDroitAPGPHelper.java
+++ webavs-web/src/main/java/globaz/apg/helpers/droits/APDroitAPGPHelper.java
@@ -311,8 +311,8 @@ public class APDroitAPGPHelper extends APAbstractDroitPHelper {
         if(JadeStringUtil.isBlankOrZero(CommonPropertiesUtils.getValue(CommonProperties.RAPG_KEYSTORE_TYPE))){
             listPropertiesEmpty.add(prefix+CommonProperties.RAPG_KEYSTORE_TYPE.getPropertyName());
         }
-        if(JadeStringUtil.isBlankOrZero(CommonPropertiesUtils.getValue(CommonProperties.RAPG_SEODOR_WSDL_PATH))){
-            listPropertiesEmpty.add(prefix+CommonProperties.RAPG_SEODOR_WSDL_PATH.getPropertyName());
+        if(JadeStringUtil.isBlankOrZero(CommonPropertiesUtils.getValue(CommonProperties.RAPG_WEBSERVICE_WSDL_PATH))){
+            listPropertiesEmpty.add(prefix+CommonProperties.RAPG_WEBSERVICE_WSDL_PATH.getPropertyName());
         }
         if(JadeStringUtil.isBlankOrZero(CommonPropertiesUtils.getValue(CommonProperties.RAPG_SSL_CONTEXT_TYPE))){
             listPropertiesEmpty.add(prefix+CommonProperties.RAPG_SSL_CONTEXT_TYPE.getPropertyName());
