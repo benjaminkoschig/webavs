1a58b5c9e7d12edd368edd8c282791e9a83efbd5
von <von@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-05-28 11:45:11 +0000

    WebAVS-Pandémie - Contentieux - Surcis Progation Taux Intérêts

--- webavs-sql/src/main/sql/osiris.sql
+++ webavs-sql/src/main/sql/osiris.sql
@@ -3,7 +3,7 @@ INSERT INTO SCHEMA.FWPARP (PPARAP,PPACDI,PPARIA,PPADDE,PPARPD,PCOSID,PPARPF,PPAR
 INSERT INTO SCHEMA.FWPARP (PPARAP,PPACDI,PPARIA,PPADDE,PPARPD,PCOSID,PPARPF,PPARVA,PPRAVN,PPRADE,CSTYUN,PCOITC,PSPY) VALUES ('OSIRIS','TAUXPAND20',0,20200321,0.000000,10200030,0.000000,'',0.000000,'Taux d''intérêts pour les cas en surcis/prorogation',0,0,'20200328101910globaz');
 INSERT INTO SCHEMA.FWPARP (PPARAP,PPACDI,PPARIA,PPADDE,PPARPD,PCOSID,PPARPF,PPARVA,PPRAVN,PPRADE,CSTYUN,PCOITC,PSPY) VALUES ('OSIRIS','TAUXPAND20',0,20200921,0.000000,10200030,0.000000,'',5.000000,'Taux d''intérêts pour les cas en surcis/prorogation',0,0,'20200328101910globaz');
 
-UPDATE SCHEMA.FWPARP SET PPADDE=20200701 WHERE PPACDI='TAUX' AND PPARAP='OSIRIS' AND PPADDE='20200921'
+UPDATE SCHEMA.FWPARP SET PPADDE=20200701 WHERE PPACDI='TAUX' AND PPARAP='OSIRIS' AND PPADDE='20200921';
 insert into SCHEMA.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy) values (237600,'OSIREFRUB',1,1,0,0,'CONTENTIEUX_INTERET_MORATOIRE_PARITAIRE',2, 2,2,2,2,2,10200037,0,'spy');
 insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (237600,'D','','[de] Contentieux - Intérêts moratoires paritaires','spy');
 insert into SCHEMA.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (237600,'F','','Contentieux - Intérêts moratoires paritaires','spy');
