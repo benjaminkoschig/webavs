901ce00ad8ed69418f47966eeeb3b581785a8042
ahe <ahe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-06-24 10:21:34 +0000

    Retrait du jadeprop ayant été déjà ajouté lors de la version 1.23.3 script pour tous sauf ferciab.sql

--- webavs-sql/src/main/sql/musca.sql
+++
@@ -1,2 +0,0 @@
--- WEBAVS-7173 : Affichage du taux moyen. Ajout d'une propriété
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.afficheTauxParPalier','false','20191216100439globaz    ','20191216100439globaz    ');
