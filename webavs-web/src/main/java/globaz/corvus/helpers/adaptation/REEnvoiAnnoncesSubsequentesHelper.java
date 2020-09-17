e201ceda107e001917bfc707f7d9ee8c7e6dc125
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-09-17 06:51:59 +0000

    Adapt rente - 4.7 : Envoi annonce XML

--- webavs-web/src/main/java/globaz/corvus/helpers/adaptation/REEnvoiAnnoncesSubsequentesHelper.java
+++ webavs-web/src/main/java/globaz/corvus/helpers/adaptation/REEnvoiAnnoncesSubsequentesHelper.java
@@ -1,16 +1,22 @@
 package globaz.corvus.helpers.adaptation;
 
+import ch.globaz.common.exceptions.CommonTechnicalException;
+import ch.globaz.common.properties.PropertiesException;
 import globaz.corvus.process.REEnvoiAnnoncesSubsequentesProcess;
+import globaz.corvus.process.REEnvoyerAnnoncesXMLProcess;
+import globaz.corvus.properties.REProperties;
 import globaz.corvus.vb.adaptation.REEnvoiAnnoncesSubsequentesViewBean;
 import globaz.framework.bean.FWViewBeanInterface;
 import globaz.framework.controller.FWAction;
 import globaz.globall.api.BISession;
+import globaz.globall.db.BSession;
+import globaz.globall.util.JACalendar;
 import globaz.prestation.helpers.PRAbstractHelper;
 
 /**
- * 
+ *
  * @author HPE
- * 
+ *
  */
 public class REEnvoiAnnoncesSubsequentesHelper extends PRAbstractHelper {
 
@@ -19,11 +25,27 @@ public class REEnvoiAnnoncesSubsequentesHelper extends PRAbstractHelper {
 
         REEnvoiAnnoncesSubsequentesViewBean vb = (REEnvoiAnnoncesSubsequentesViewBean) viewBean;
 
-        REEnvoiAnnoncesSubsequentesProcess process = new REEnvoiAnnoncesSubsequentesProcess();
-        process.setSession(vb.getSession());
-        process.setEMailAddress(vb.getEMailAddress());
-        process.setMoisAnnee(vb.getMoisAnnee());
-        process.start();
+        // D0215 si la propriété est activée on passe en mode xml
+        try {
+            if (REProperties.ACTIVER_ANNONCES_XML.getBooleanValue()) {
+                REEnvoyerAnnoncesXMLProcess process = new REEnvoyerAnnoncesXMLProcess((BSession) session);
+                process.setEMailAddress(vb.getEMailAddress());
+                process.setForDateEnvoi(JACalendar.todayJJsMMsAAAA());
+                process.setForMoisAnneeComptable(vb.getMoisAnnee());
+                process.setIsForAnnoncesSubsequentes(true);
+                process.start();
+
+            } else {
+
+                REEnvoiAnnoncesSubsequentesProcess process = new REEnvoiAnnoncesSubsequentesProcess();
+                process.setSession(vb.getSession());
+                process.setEMailAddress(vb.getEMailAddress());
+                process.setMoisAnnee(vb.getMoisAnnee());
+                process.start();
+            }
+        } catch (PropertiesException e) {
+            throw new CommonTechnicalException("Impossible de récupérer la propriété corvus.isAnnoncesXML", e);
+        }
 
     }
 
