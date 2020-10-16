628259fb8fd31ec539de6d8b65d963df8d17101e
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-10-16 13:24:16 +0000

    add spy

--- webavs-sql/src/main/sql/draco.sql
+++ webavs-sql/src/main/sql/draco.sql
@@ -3,5 +3,5 @@ INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY) VALUES ('draco.major
 INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY) VALUES ('draco.majoration.declaration.manuelle.assurance', '', '20200915103000globaz', '20200915103000globaz');
 
 INSERT INTO SCHEMA.FWCOSP(pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise, pspy) values (812028,'VETYPEASSU',1,1,0,0,'Frais_d_administration_maj',2,1,2,2,2,2,10800012,0, '20200915103000globaz');
-INSERT INTO SCHEMA.FWCOUP(pcosid,plaide,pcouid,pcolut, pspy)values (812028,'F','E','Frais d¬administration majoration','spy');
-INSERT INTO SCHEMA.FWCOUP(pcosid,plaide,pcouid,pcolut, pspy)values (812028,'D','FAD','[DE]Verwaltungskosten Zuschlag','spy');
+INSERT INTO SCHEMA.FWCOUP(pcosid,plaide,pcouid,pcolut, pspy)values (812028,'F','E','Frais d¬administration majoration','20200915103000globaz');
+INSERT INTO SCHEMA.FWCOUP(pcosid,plaide,pcouid,pcolut, pspy)values (812028,'D','FAD','[DE]Verwaltungskosten Zuschlag','20200915103000globaz');
