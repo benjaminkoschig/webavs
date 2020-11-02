885cc40aa8ca3a810a145adb58743d3aaeb72d5d
ahe <ahe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-11-02 10:51:34 +0000

    Déplacement dans scripts spécifiques dès 1.25.0-RC2 POUR FERCIAB

--- webavs-sql/src/main/sql/draco.sql
+++
@@ -1,4 +0,0 @@
--- [WEBAVS-8330] FERCIAB - S190902_006
-INSERT INTO SCHEMA.FWCOSP(pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise, pspy) values (812043,'VETYPEASSU',1,1,0,0,'Frais_d_administration_maj',2,1,2,2,2,2,10800012,0, '20200915103000globaz');
-INSERT INTO SCHEMA.FWCOUP(pcosid,plaide,pcouid,pcolut, pspy)values (812043,'F','E','Frais d¬administration majoration','20200915103000globaz');
-INSERT INTO SCHEMA.FWCOUP(pcosid,plaide,pcouid,pcolut, pspy)values (812043,'D','FAD','[DE]Verwaltungskosten Zuschlag','20200915103000globaz');
