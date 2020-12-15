e9cc82dd1d595072e01a6a3837b782cc08da6c76
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2020-12-15 12:53:19 +0000

    [WEBAVS-8330] FERCIAB - S190902_006 -> Oubli de mettre la propriété dans la version 1.25.0 PROD

---
+++ webavs-sql/src/main/sql/draco.sql
@@ -0,0 +1,3 @@
+-- [WEBAVS-8330] FERCIAB - S190902_006
+INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY) VALUES ('draco.majoration.declaration.manuelle.active', '', '20201215103000globaz', '20201215103000globaz');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY) VALUES ('draco.majoration.declaration.manuelle.assurance', '', '20201215103000globaz', '20201215103000globaz');
