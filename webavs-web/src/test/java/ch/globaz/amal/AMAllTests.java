5bd7aa6a8856b149876994fdf839a63fa2415fe4
dma <dma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-06-01 11:40:58 +0000

    quality(test): Suppression de class de tests qui ne sont pas utilisé
    ref PLAT1-22

--- webavs-web/src/test/java/ch/globaz/amal/AMAllTests.java
+++ webavs-web/src/test/java/ch/globaz/amal/AMAllTests.java
@@ -1,21 +1,20 @@
 /**
- * 
+ *
  */
 package ch.globaz.amal;
 
+import ch.globaz.amal.businessimpl.services.libra.SuiteTestsServicesLibra;
+import ch.globaz.amal.businessimpl.services.models.SuiteTestsServiceModels;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import ch.globaz.amal.businessimpl.services.libra.SuiteTestsServicesLibra;
-import ch.globaz.amal.businessimpl.services.models.SuiteTestsServiceModels;
-import ch.globaz.amal.businessimpl.services.pyxis.SuiteTestsServicesPyxis;
 
 /**
  * @author DHI
- * 
+ *
  */
 @RunWith(Suite.class)
-@SuiteClasses(value = { SuiteTestsServiceModels.class, SuiteTestsServicesPyxis.class, SuiteTestsServicesLibra.class })
+@SuiteClasses(value = {SuiteTestsServiceModels.class, SuiteTestsServicesLibra.class})
 public class AMAllTests {
 
 }
