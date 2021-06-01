8ba9ebd2a8af25cb82cfc298582fa35af7c57476
ebko <ebko@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-06-01 07:05:46 +0000

    feat(cotisations personnelles): adaptation de la gestion des cotisations personnelles AVS
    ref #S201029_007

---
+++ webavs-sql/src/main/sql/phenix.sql
@@ -0,0 +1,4 @@
+
+-- S201029_007 : Gestion du taux des cotisations personnelles
+INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY)
+VALUES ('phenix.idAssuranceAVS', '', '202106012120000Globaz    ', '202106012120000Globaz    ');
