9ceac9a582b23c4bbc0f6651238cfe44c15adaa9
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-08-11 14:50:40 +0000

    feat(Vidange script): repo local mal rafraichit

--- webavs-sql/src/main/sql/phenix.sql
+++
@@ -1,4 +0,0 @@
-
--- S201029_007 : Gestion du taux des cotisations personnelles
-INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY)
-VALUES ('phenix.idAssuranceAVS', '', '202106012120000Globaz    ', '202106012120000Globaz    ');
