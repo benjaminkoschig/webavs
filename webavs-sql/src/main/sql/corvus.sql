c7153611e464bb0d7c0cfc2e74fe758065813244
mpe <mpe@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-08-11 11:58:59 +0000

    fix(vidange script): vidange des scripts

--- webavs-sql/src/main/sql/corvus.sql
+++
@@ -1,5 +0,0 @@
--- WEBAVS-7919 Ajout des propriétés pour ACOR V4acor.re.factory.class
-
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('corvus.acor.utiliser.version.web','false','20200526120000Globaz    ','20200526120000Globaz    ');
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('corvus.acor.adresse.web','','20200526120000Globaz    ','20200526120000Globaz    ');
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('corvus.acor.navigateur','','20200526120000Globaz    ','20200526120000Globaz    ');
