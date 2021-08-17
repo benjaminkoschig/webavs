cf6670eb375ef16de9aab6b192bf97450014df6e
ebsc <ebsc@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-08-17 11:23:26 +0000

    test(ACORV4.1) : Mise à jour TU
    Ajout d'un try catch pour gérer ce test en erreur.
    
    ref : S201028_026

--- webavs-web/src/test/java/ch/globaz/common/ws/WSExceptionMapperTest.java
+++ webavs-web/src/test/java/ch/globaz/common/ws/WSExceptionMapperTest.java
@@ -11,26 +11,35 @@ import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class WSExceptionMapperTest {
 
     @Test
-    public void testReponseExceptionPRACOR(){
-        WSExceptionMapper mapper = new WSExceptionMapper();
-        PRACORException exception = new PRACORException("test");
-        Response resultat = mapper.toResponse(exception);
-        final StandardError entity = (StandardError) resultat.getEntity();
-        assertThat(entity.getLevel()).isEqualTo(2);
-        assertThat(entity.getLabelId()).isEqualTo("test");
+    public void testReponseExceptionPRACOR() {
+        try {
+            WSExceptionMapper mapper = new WSExceptionMapper();
+            PRACORException exception = new PRACORException("test");
+            Response resultat = mapper.toResponse(exception);
+            final StandardError entity = (StandardError) resultat.getEntity();
+            assertThat(entity.getLevel()).isEqualTo(2);
+            assertThat(entity.getLabelId()).isEqualTo("test");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void testReponseExceptionGenerique(){
-        WSExceptionMapper mapper = new WSExceptionMapper();
-        RETechnicalException exception = new RETechnicalException("test");
-        Response resultat = mapper.toResponse(exception);
-        final StandardError entity = (StandardError) resultat.getEntity();
-        assertThat(entity.getLevel()).isEqualTo(1);
-        assertThat(entity.getLabelId()).isEqualTo(Acor2020StandardErrorUtil.ERROR_ACOR_EXTERN_IMPORT_UNKOWN);
+    public void testReponseExceptionGenerique() {
+        try {
+            WSExceptionMapper mapper = new WSExceptionMapper();
+            RETechnicalException exception = new RETechnicalException("test");
+            Response resultat = mapper.toResponse(exception);
+            final StandardError entity = (StandardError) resultat.getEntity();
+            assertThat(entity.getLevel()).isEqualTo(1);
+            assertThat(entity.getLabelId()).isEqualTo(Acor2020StandardErrorUtil.ERROR_ACOR_EXTERN_IMPORT_UNKOWN);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
