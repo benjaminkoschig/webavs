efa07c25af28f2b85476498ea80a5217a120fc72
dma <dma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-08-19 15:36:12 +0000

    refactor: adaptation pour websphere
    ref: WEBAVS-9937

--- webavs-web/src/main/java/ch/globaz/common/ws/WSExceptionMapper.java
+++ webavs-web/src/main/java/ch/globaz/common/ws/WSExceptionMapper.java
@@ -28,23 +28,26 @@ public class WSExceptionMapper implements ExceptionMapper<Exception> {
     @Override
     public Response toResponse(Exception e) {
         LOG.error("WebService Error : ", e);
-        LOG.info("getRequestURI : {}",request.getRequestURI());
-        LOG.info("getContextPath : {}",request.getContextPath());
-        LOG.info("getPathInfo : {}",request.getPathInfo());
-        LOG.info("getQueryString : {}",request.getQueryString());
-        LOG.info("getAuthType : {}",request.getAuthType());
-        LOG.info("getRemoteUser : {}",request.getRemoteUser());
-        LOG.info("getMethod : {}",request.getMethod());
-        LOG.info("getLocalAddr : {}",request.getLocalAddr());
-        LOG.info("getRequestedSessionId : {}",request.getRequestedSessionId());
-        LOG.info("getServerName : {}",request.getServerName());
+        LOG.info("RequestURI : {}",request.getRequestURI());
+        LOG.info("ContextPath : {}",request.getContextPath());
+        LOG.info("PathInfo : {}",request.getPathInfo());
+        LOG.info("QueryString : {}",request.getQueryString());
+        LOG.info("AuthType : {}",request.getAuthType());
+        LOG.info("RemoteUser : {}",request.getRemoteUser());
+        LOG.info("Method : {}",request.getMethod());
+        LOG.info("LocalAddr : {}",request.getLocalAddr());
+        LOG.info("RequestedSessionId : {}",request.getRequestedSessionId());
+        LOG.info("ServerName : {}",request.getServerName());
+        LOG.info("LocalAddr : {}",request.getLocalAddr());
+        LOG.info("RemoteAddr : {}",request.getRemoteAddr());
+        LOG.info("Scheme : {}",request.getScheme());
 
-        LOG.info("getPath : {}",uriInfo.getPath());
-        LOG.info("getPath : {}",uriInfo.getPath());
-        LOG.info("getRequestUri : {}",uriInfo.getRequestUri().toString());
-        LOG.info("getBaseUri : {}",uriInfo.getBaseUri().toString());
-        LOG.info("getAbsolutePath : {}",uriInfo.getAbsolutePath().toString());
-        LOG.info("getPathSegments : {}",uriInfo.getPathSegments().toString());
+        LOG.info("Path : {}",uriInfo.getPath());
+        LOG.info("RequestUri : {}",uriInfo.getRequestUri().toString());
+        LOG.info("BaseUri : {}",uriInfo.getBaseUri().toString());
+        LOG.info("AbsolutePath : {}",uriInfo.getAbsolutePath().toString());
+        LOG.info("PathSegments : {}",uriInfo.getPathSegments().toString());
+
         Response.ResponseBuilder responseBuilder = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
         responseBuilder.type(MediaType.APPLICATION_JSON_TYPE);
         if (e instanceof PRACORException) {
