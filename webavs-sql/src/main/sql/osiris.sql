3d6d8f90fe1b3cab9f32ab8d60d11b7385240eee
eniv <eniv@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-09-22 06:14:19 +0000

    fix : suppression script qui n'avait rien à  faire là
    suppression des lignes
    
    ref :

--- webavs-sql/src/main/sql/osiris.sql
+++ webavs-sql/src/main/sql/osiris.sql
@@ -1,9 +1,3 @@
--- POAVS-2955 - NE PAS EXECUTER SUR CCJU
-ALTER TABLE SCHEMA.CGJOURP ADD COLUMN ISEXTRACT CHAR(1) DEFAULT 1;
-
-REORG TABLE SCHEMA.CGJOURP;
-
--- FIN NON EXECUTION SUR CCJU
 insert into SCHEMA.FWCOUP (PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY) values (223009, 'F', 'AVNPTRA   ', 'Avance PTRA                                                 ', '                        ');
 insert into SCHEMA.FWCOUP (PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY) values (223009, 'D', 'AVNPTRA', 'Vorauszahlung ÜL', '');
 insert into SCHEMA.FWCOSP (PCOSID, PPTYGR, PCONCS, PPTYCN, PPTYCL, PPTYSA, PCOSLI, PCOSDF, PCOSDM, PCOSDP, PCOIAN, PCOIDE, PCODFI, PCOITC, PCOISE, PSPY) values (223009, 'OSIPLRMOD ', 1, 1, 0, 0, 'Avance PTRA                             ', 2, 1, 2, 2, 2, 2, 10200023, 0, '                        ');
