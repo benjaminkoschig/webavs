d9a90c86398db58e8a41f332dba8ea663d8a928c
dma <dma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-10-01 07:26:45 +0000

    fix: test pour websphere

--- webavs-web/src/main/java/ch/globaz/common/ws/configuration/HealthApiRest.java
+++ webavs-web/src/main/java/ch/globaz/common/ws/configuration/HealthApiRest.java
@@ -3,7 +3,6 @@ package ch.globaz.common.ws.configuration;
 import ch.globaz.common.ws.HealthDto;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,13 +13,13 @@ import java.util.List;
 @Slf4j
 @Path("/infos")
 @Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class HealthApiRest {
 
     @GET
     @Path("/health")
     public Response health() {
-        return Response.ok(new ApiHealthCheckerService().checkApi()).build();
+        return Response.ok(new ApiHealthCheckerService().checkApi())
+                .build();
     }
 
     @GET
@@ -30,25 +29,36 @@ public class HealthApiRest {
     }
 
     @GET
-    @Path(value= "/healthv")
+    @Path(value = "/healthv")
     public Response testv() {
-        return Response.ok(new ApiHealthCheckerService().checkApi()).build();
+        return Response.ok(new ApiHealthCheckerService().checkApi())
+                .build();
     }
 
     @GET
-    @Path(value= "/healthv2")
-    public  List<HealthDto> testv2() {
+    @Path(value = "/healthv2")
+    public List<HealthDto> testv2() {
         return new ApiHealthCheckerService().checkApi();
     }
 
     @GET
     @Path("/health2")
     public Response health2() {
-        return Response.ok(new ApiHealthCheckerService().checkApi()).build();
+        return Response.ok(new ApiHealthCheckerService().checkApi())
+                .build();
+    }
+
+
+    @GET
+    @Path("/health3")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String health3() {
+        return "Test";
     }
 
     @GET
     public Response health1() {
-        return Response.ok(new ApiHealthCheckerService().checkApi()).build();
+        return Response.ok(new ApiHealthCheckerService().checkApi())
+                .build();
     }
 }
