81a014adb5a3d1c284ef97c11cb3c3a0842e1055
jjo <jjo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-10-05 10:29:43 +0000

    fix : mise à jour script sql PSPY
    User du PSPY setté à globaz.

--- webavs-sql/src/main/sql/corvus.sql
+++ webavs-sql/src/main/sql/corvus.sql
@@ -9,14 +9,14 @@ REORG table SCHEMA.RECREAN;
 -- POAVS-3405 ENSAVS - S201028_026 - ACOR V4.1 - Gestion de la fraction de rente
 -- Nouveau cas spécial 33
 insert into schema.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy)
-                values (52822033,'RECASSPEC',33,1,0,0,'CAS_SPECIAUX_RENTE_33',2, 1,2,2,2,2,51800022,0,VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat user);
-insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822033,'D','33','Code 33',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat user);
-insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822033,'F','33','Code 33',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat user);
+                values (52822033,'RECASSPEC',33,1,0,0,'CAS_SPECIAUX_RENTE_33',2, 1,2,2,2,2,51800022,0,VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822033,'D','33','Code 33',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822033,'F','33','Code 33',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
 -- Nouveau cas spécial 85
 insert into schema.fwcosp (pcosid,pptygr,pconcs,pptycn,pptycl,pptysa,pcosli,pcosdf,pcosdm,pcosdp,pcoian,pcoide,pcodfi,pcoitc,pcoise,pspy)
-values (52822085,'RECASSPEC',85,1,0,0,'CAS_SPECIAUX_RENTE_85',2, 1,2,2,2,2,51800022,0,VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat user);
-insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822085,'D','85','Code 85',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat user);
-insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822085,'F','85','Code 85',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat user);
+values (52822085,'RECASSPEC',85,1,0,0,'CAS_SPECIAUX_RENTE_85',2, 1,2,2,2,2,51800022,0,VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822085,'D','85','Code 85',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+insert into schema.fwcoup (pcosid,plaide,pcouid,pcolut,pspy) values (52822085,'F','85','Code 85',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
 
 -- Nouvelle colonne dans table Prestation Accordée REPRACC
 ALTER TABLE schema.REPRACC
