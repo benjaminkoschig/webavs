80629d5a479137bc786ecf1f729aa76c0569634d
dma <dma@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-10-07 13:59:07 +0000

    fix: correction que le buid pass
    ref : WEBAVS-10577

--- webavs-web/src/main/java/ch/globaz/common/properties/CommonProperties.java
+++ webavs-web/src/main/java/ch/globaz/common/properties/CommonProperties.java
@@ -27,7 +27,7 @@ public enum CommonProperties implements IProperties {
      * listes des prestations (PC, RFM, IJ, RENTES)
      */
     ADD_COMMUNE_POLITIQUE("liste.ajouter.commune.politique",
-            "Enrichit une partie des listes PC, RFM, IJ et rentes avec le commune politiques"),
+                          "Enrichit une partie des listes PC, RFM, IJ et rentes avec le commune politiques"),
     WIRRWEBSERVICE_NAMESPACE("wirr.webservice.namespace", "name space du web service wirr"),
     WIRRWEBSERVICE_NAME("wirr.webservice.name", "name du web service wirr"),
     WIRRWEBSERVICE_SEDEX_SENDER_ID("wirr.webservice.sedex.sender.id", "sedex sender id pour le web service wirr"),
@@ -104,6 +104,10 @@ public enum CommonProperties implements IProperties {
 
     public Integer getIntegerWithDefaultValue(Integer defaultValue)  {
         return CommonPropertiesUtils.getIntegerWithDefaultValue(this, String.valueOf(defaultValue));
+    }
+
+    public boolean exist() {
+        return CommonPropertiesUtils.exist(this);
     }
 
     @Override
