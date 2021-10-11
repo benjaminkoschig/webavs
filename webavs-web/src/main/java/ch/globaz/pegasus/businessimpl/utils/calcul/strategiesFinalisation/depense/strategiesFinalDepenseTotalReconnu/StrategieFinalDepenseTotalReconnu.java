d0f53cae4069e5594e4887317478ee12570f1655
eniv <eniv@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-10-11 10:10:28 +0000

    fix - Prest Calcul pour personnes incarcérées
    specificité CCJU - AVS et AI argent de poche pour SPEN
    
    ref : PLAT2-591/POAVS-3768

--- webavs-web/src/main/java/ch/globaz/pegasus/businessimpl/utils/calcul/strategiesFinalisation/depense/strategiesFinalDepenseTotalReconnu/StrategieFinalDepenseTotalReconnu.java
+++ webavs-web/src/main/java/ch/globaz/pegasus/businessimpl/utils/calcul/strategiesFinalisation/depense/strategiesFinalDepenseTotalReconnu/StrategieFinalDepenseTotalReconnu.java
@@ -1,18 +1,23 @@
 package ch.globaz.pegasus.businessimpl.utils.calcul.strategiesFinalisation.depense.strategiesFinalDepenseTotalReconnu;
 
+import ch.globaz.pegasus.business.constantes.IPCHomes;
 import ch.globaz.pegasus.business.constantes.IPCValeursPlanCalcul;
 import ch.globaz.pegasus.business.exceptions.models.calcul.CalculBusinessException;
 import ch.globaz.pegasus.business.exceptions.models.calcul.CalculException;
+import ch.globaz.pegasus.business.models.calcul.CalculDonneesHome;
 import ch.globaz.pegasus.businessimpl.utils.calcul.CalculContext;
 import ch.globaz.pegasus.businessimpl.utils.calcul.CalculContext.Attribut;
 import ch.globaz.pegasus.businessimpl.utils.calcul.TupleDonneeRapport;
+import ch.globaz.pegasus.businessimpl.utils.calcul.TypeRenteMap;
 import ch.globaz.pegasus.businessimpl.utils.calcul.containercalcul.ControlleurVariablesMetier;
 import ch.globaz.pegasus.businessimpl.utils.calcul.strategiesFinalisation.StrategieCalculFinalisation;
+import ch.globaz.pegasus.utils.PCApplicationUtil;
 import globaz.jade.client.util.JadeDateUtil;
 import globaz.jade.log.JadeLogger;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class StrategieFinalDepenseTotalReconnu implements StrategieCalculFinalisation {
     protected final static String[] champs = { IPCValeursPlanCalcul.CLE_DEPEN_BES_VITA_TOTAL,
@@ -204,19 +209,84 @@ public class StrategieFinalDepenseTotalReconnu implements StrategieCalculFinalis
 
         }
 
+        Attribut attribut = null;
         // valeur par defaut 0, pas de depense, dans le cas ou la personne n'est
         // pas dans un home
         Float depensesPersonnelles = 0f;
 
-        try {
-            depensesPersonnelles = ArgentDePocheHomeResolver.getMontantForCalcul(context, csTypeChambre);
-        } catch (Exception e) {
-            JadeLogger.info(this, e.getMessage());
-            throw new CalculBusinessException(
-                    "pegasus.calcul.strategie.final.depenseTotalReconnu.csTypeChambre.integrity", csTypeChambre,
-                    (String) context.get(Attribut.DATE_DEBUT_PERIODE));
+        // stocke temporairement le type de chambre pour la strategie finale qui calcule les dépenses personnelles
+        String csPeriodeServiceEtat = donnee.getLegendeEnfant(IPCValeursPlanCalcul.CLE_INTER_DEPENSE_CS_PERIODE_SERVICE_ETAT);
+
+        // Ajout du canton du Jura, recuperation argent de poche Home annuel pour Jura
+        if (PCApplicationUtil.isCantonJU() && IPCHomes.CS_SERVICE_ETAT_SPEN.equals(csPeriodeServiceEtat)) {
+            String csTypeRentePC = getCsTypeRentePC(donnee, context);
+            if (TypeRenteMap.listeCsRenteSurvivant.contains(csTypeRentePC)
+                    || TypeRenteMap.listeCsRenteInvalidite.contains(csTypeRentePC)) {
+                attribut = Attribut.CS_ARGENT_POCHE_HOME_AI_ANNUEL;
+            } else if (TypeRenteMap.listeCsRenteVieillesse.contains(csTypeRentePC)) {
+                attribut = Attribut.CS_ARGENT_POCHE_HOME_AVS_ANNUEL;
+            }
+
+            try {
+                depensesPersonnelles = Float.parseFloat(((ControlleurVariablesMetier) context.get(attribut))
+                        .getValeurCourante());
+            } catch (Exception e) {
+                JadeLogger.info(this, e.getMessage());
+                throw new CalculBusinessException(
+                        "pegasus.calcul.strategie.final.depenseTotalReconnu.csTypeRentePC.integrity", csTypeRentePC,
+                        (String) context.get(Attribut.DATE_DEBUT_PERIODE));
+            }
+
+        } else {
+            try {
+                depensesPersonnelles = ArgentDePocheHomeResolver.getMontantForCalcul(context, csTypeChambre);
+            } catch (Exception e) {
+                JadeLogger.info(this, e.getMessage());
+                throw new CalculBusinessException(
+                        "pegasus.calcul.strategie.final.depenseTotalReconnu.csTypeChambre.integrity", csTypeChambre,
+                        (String) context.get(Attribut.DATE_DEBUT_PERIODE));
+            }
         }
+
         return depensesPersonnelles * 12;
+    }
+
+    // Retourne le sinformations du home (plus psécifiquement du type d echambre) correspondant à celui de la donnée
+    // financière
+    private CalculDonneesHome getHomeForTypeChambre(List<CalculDonneesHome> donneesHomes, String idTypeChambre) {
+
+        for (CalculDonneesHome home : donneesHomes) {
+            if (idTypeChambre.equals(home.getIdTypeChambre())) {
+                return home;
+            }
+        }
+        // home pas trouvé
+        return null;
+    }
+
+    private String getCsTypeRentePC(TupleDonneeRapport donnee, CalculContext context) {
+        String csTypeRentePC = null;
+
+        try {
+            csTypeRentePC = donnee.getLegendeEnfant(IPCValeursPlanCalcul.CLE_INTER_TYPE_RENTE_REQUERANT);
+
+            // fallback sur le conjoint
+            if (csTypeRentePC == null) {
+                csTypeRentePC = donnee.getLegendeEnfant(IPCValeursPlanCalcul.CLE_INTER_TYPE_RENTE_CONJOINT);
+
+                if (null == csTypeRentePC) {
+                    // Si toujours null, on va rechercher le type de rente du requérant dans le contete
+                    csTypeRentePC = (String) context.get(Attribut.TYPE_RENTE_REQUERANT);
+                }
+
+            }
+
+        } catch (NullPointerException | CalculException e) {
+            JadeLogger.info(this,
+                    "The value csTypeRentePC comming from calculator is null. Meaning the people is not in a home.");
+        }
+
+        return csTypeRentePC;
     }
 
     protected void checkSejourMoisPartiel(CalculContext context, TupleDonneeRapport donnee) throws CalculException{
