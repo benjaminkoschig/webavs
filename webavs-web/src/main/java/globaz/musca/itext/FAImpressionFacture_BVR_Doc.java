5a13ff22b78dc60ff06b4393fab12dc79b1cd5ab
ebsc <ebsc@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2021-11-11 16:06:55 +0000

    feat(QR facture) : Contrôle du bulletin neutre dans la facturation.
    ref : WEBAVS-10976

--- webavs-web/src/main/java/globaz/musca/itext/FAImpressionFacture_BVR_Doc.java
+++ webavs-web/src/main/java/globaz/musca/itext/FAImpressionFacture_BVR_Doc.java
@@ -747,6 +747,9 @@ public class FAImpressionFacture_BVR_Doc extends FAImpressionFacturation {
             qrFacture.setSession(getSession());
             // Initialisation des variables du document
             initVariableQR(currentDataSource.getEnteteFacture().getISOLangueTiers());
+
+            qrFacture.setQrNeutre(APISection.ID_TYPE_SECTION_BULLETIN_NEUTRE.equals(currentDataSource.getEnteteFacture().getIdTypeFacture()));
+
             // Génération du document QR
             qrFacture.initQR(this);
 
