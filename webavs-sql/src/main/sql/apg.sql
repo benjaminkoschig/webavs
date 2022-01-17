2a7114e406b997b527992ab896c29e6f8404cf0d
jjo <jjo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2022-01-17 08:24:40 +0000

    [new-version] Vidange des script SQL pour version 1.28.0 sur le trunk.

--- webavs-sql/src/main/sql/apg.sql
+++
@@ -1,5 +0,0 @@
--- ENSAVS-S210924_008-SPE-APG-uniformisation statuts dossiers
--- Suppression des code système pour l'état de droit APG Enregistré
--- A exécuter pour toutes les caisses.
-delete from schema.FWCOUP where PCOSID = (select PCOSID from schema.FWCOSP where PPTYGR like 'APETADROIT' AND PCONCS = 9);
-delete from schema.FWCOSP where PPTYGR like 'APETADROIT' AND PCONCS = 9;
