2b342a393d8bd04aba5e217f54411f07c06efa48
jjo <jjo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2022-01-26 12:52:37 +0000

    feat(eFormulaire P14) Mise en place DB
    Ajout de la table GF_FORMULAIRE et des jade prop nécessaire au CRON
    
    ref : S210924_002, WEBAVS-11317

--- webavs-sql/src/main/sql/eform.sql
+++ webavs-sql/src/main/sql/eform.sql
@@ -1,2 +1,23 @@
--- ajout jade prop application class name
-insert into SCHEMA.JADEPROP (PROPNAME, PROPVAL) VALUES ('eform.applicationClasseName', 'ch.globaz.eform.web.application.GFApplication');
+-- ajout des propriété jade pour module eFormulaire P14
+-- Application class name
+insert into SCHEMA.JADEPROP (PROPNAME, PROPVAL) VALUES ('eform.applicationClassName', 'ch.globaz.eform.web.application.GFApplication');
+
+-- Répertoires pour l'importation des eFormulaires P14
+-- Répertoire d'importation du SM client
+insert into SCHEMA.JADEPROP (PROPNAME, PROPVAL) values ('eform.path.sm.client.form', '/job/jobadm1/SedexBackup/inbox/ok/');
+-- Répertoire d'archivage des fichiers sedex P14 importés
+insert into SCHEMA.JADEPROP (PROPNAME, PROPVAL) values ('eform.path.storage.form', '/job/jobadm1/batch/eform/');
+
+-- Création table de gestion des eFormulaires P14
+CREATE TABLE SCHEMA.GF_FORMULAIRE (
+    FORMULAIRE_ID varchar(50),
+    FORMULAIRE_SUBJECT varchar(8),
+    FORMULAIRE_DATE varchar(10),
+    FORMULAIRE_TYPE varchar(8),
+    FORMULAIRE_NOM varchar(50),
+    BENEFICIAIRE_NSS varchar(16),
+    BENEFICIAIRE_NOM varchar(40),
+    BENEFICIAIRE_PRENOM varchar(40),
+    BENEFICIAIRE_DATE_NAISSANCE varchar(10),
+    FICHIER_ZIP BLOB(512000)
+)
