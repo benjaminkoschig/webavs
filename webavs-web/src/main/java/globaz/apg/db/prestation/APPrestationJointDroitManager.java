b79b185773142a0774b78ee8988cb99427012b25
jjo <jjo@eab8f488-0ceb-11e0-94c3-cbab4eda1835>
2022-01-28 07:42:30 +0000

    fix(uniformisation status droit APG) correctif génération lot
    Correction de la méthode de génération des lots avec prise en compte des droit en état partielle
    
    ref: POAVS-4042, WEBAVS-11542

--- webavs-web/src/main/java/globaz/apg/db/prestation/APPrestationJointDroitManager.java
+++ webavs-web/src/main/java/globaz/apg/db/prestation/APPrestationJointDroitManager.java
@@ -11,10 +11,10 @@ import java.util.List;
 public class APPrestationJointDroitManager extends APPrestationManager {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
-    private String forEtatDroit;
+    private List<String> notForEtatDroit = new ArrayList();
 
     @Override
     protected String _getOrder(BStatement statement) {
@@ -50,13 +50,21 @@ public class APPrestationJointDroitManager extends APPrestationManager {
             sql += superWhere;
         }
 
-        if (!getForEtatDroit().isEmpty()) {
+        if (!getNotForEtatDroit().isEmpty()) {
             if (sql.length() != 0) {
                 sql += " AND ";
             }
 
+            StringBuilder valuesStr = new StringBuilder();
+            for (String genre : getNotForEtatDroit()) {
+                if (!JadeStringUtil.isEmpty(valuesStr.toString())) {
+                    valuesStr.append(",");
+                }
+                valuesStr.append(this._dbWriteNumeric(statement.getTransaction(), genre));
+            }
+
             sql += _getCollection() + APDroitLAPG.TABLE_NAME_LAPG + "." + APDroitLAPG.FIELDNAME_ETAT
-                    + " = " + getForEtatDroit();
+                    + " NOT IN (" + valuesStr.toString() + ")";
         }
 
         return sql;
@@ -68,11 +76,11 @@ public class APPrestationJointDroitManager extends APPrestationManager {
         return new APPrestation();
     }
 
-    public String getForEtatDroit() {
-        return forEtatDroit;
+    public List<String> getNotForEtatDroit() {
+        return notForEtatDroit;
     }
 
-    public void setForEtatDroit(String forEtatDroit) {
-        this.forEtatDroit = forEtatDroit;
+    public void setNotForEtatDroit(List<String> notForEtatDroit) {
+        this.notForEtatDroit = notForEtatDroit;
     }
 }
