cdb092e82b765b30d626b72724239f3ad03fa074
dma <dorian.magnin@globaz.ch>
2022-04-04 09:48:14 +0200

    🐛 changment de l'encodage  Cp1252 -> iso-8859-1
    ref: PLAT1-114,MIG-2608

--- webavs-sql/src/ant/FusionSql.groovy
+++ webavs-sql/src/ant/FusionSql.groovy
@@ -14,8 +14,8 @@ class FusionSql {
 
     public FusionSql(currentVersion, sourceSqlDir, targetSqlDir) {
         super()
-       /* System.setProperty("file.encoding","Cp1252")
-        System.setProperty("sun.jnu.encoding","Cp1252")
+       /* System.setProperty("file.encoding","iso-8859-1")
+        System.setProperty("sun.jnu.encoding","iso-8859-1")
         System.setProperty("user.timezone","Europe/Zurich")
         System.setProperty("user.country","CH")
         */
