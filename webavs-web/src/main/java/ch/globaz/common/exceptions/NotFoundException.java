38ca60523e2a475400d923b43b30e26046a6fa9e
TPA <vRb07cvp2B>
2022-04-11 11:41:15 +0200

    feature(WEBAVS-11836): correction du pipeline
    Ref: S210924_002

--- webavs-web/src/main/java/ch/globaz/common/exceptions/NotFoundException.java
+++ webavs-web/src/main/java/ch/globaz/common/exceptions/NotFoundException.java
@@ -1,6 +1,6 @@
 package ch.globaz.common.exceptions;
 
-public class NotFoundException extends Exception {
+public class NotFoundException extends RuntimeException {
    public NotFoundException(String msg) {
         super(msg);
     }
