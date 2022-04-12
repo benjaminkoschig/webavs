98907ae92df0fe325d0ff3d175e241febe49b7ff
ebko <Kleemon32>
2022-04-12 15:16:42 +0200

    feat(WEBAVS-11714): Seodor Mise à jour XSD et WSDL
    Suppression des xsd mis dans external-xsd
    
    Ref: S211221_003

--- webavs-sql/src/main/sql/apg.sql
+++ webavs-sql/src/main/sql/apg.sql
@@ -1,3 +1,18 @@
 -- Mise à jour des propriétés SEODOR
 UPDATE SCHEMA.JADEPROP SET PROPVAL = 'ServicePeriodsService-2-0', PSPY = to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz' WHERE PROPNAME = 'common.seodor.webservice.name';
 UPDATE SCHEMA.JADEPROP SET PROPVAL = 'http://www.zas.admin.ch/seodor/ws/service-periods/2', PSPY = to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz' WHERE PROPNAME = 'common.seodor.webservice.namespace';
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('common.es.token.duration','1',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+
+-- Incident I220309_015
+ALTER TABLE SCHEMA.CAEI ALTER NOM SET DATA TYPE varchar(70);
+ALTER TABLE SCHEMA.CAEI ALTER PRENOM SET DATA TYPE varchar(70);
+ALTER TABLE SCHEMA.CAEI ALTER ENTREPRISE SET DATA TYPE varchar(70);
+-- reorg table SCHEMA.CAEI;
+
+ALTER TABLE SCHEMA.CAET ALTER PRENOM SET DATA TYPE varchar(70);
+ALTER TABLE SCHEMA.CAET ALTER ENTREPRISE SET DATA TYPE varchar(70);
+ALTER TABLE SCHEMA.CAET ALTER NOM SET DATA TYPE varchar(70);
+-- reorg table SCHEMA.CAET;
+
+--eBill
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('osiris.eBill.email.confirmation','', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
