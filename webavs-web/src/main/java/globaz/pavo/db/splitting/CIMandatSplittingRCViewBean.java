72051867bc5a1c103cc409e09590fbc943b04425
ebko <Kleemon32>
2022-05-04 12:04:41 +0200

    feat(WEBAVS-11884) Splitting - Ajout option GED Ajout lien ged dans les options mandat de splitting
    Ref: S210527_007

--- webavs-web/src/main/java/globaz/pavo/db/splitting/CIMandatSplittingRCViewBean.java
+++ webavs-web/src/main/java/globaz/pavo/db/splitting/CIMandatSplittingRCViewBean.java
@@ -1,6 +1,8 @@
 package globaz.pavo.db.splitting;
 
 import globaz.framework.bean.FWViewBeanInterface;
+import globaz.jade.client.util.JadeStringUtil;
+import globaz.pavo.application.CIApplication;
 
 /**
  * ViewBean pour l'en-tête de la gestion des mandats. Date de création : (29.10.2002 09:14:22)
@@ -22,5 +24,19 @@ public class CIMandatSplittingRCViewBean extends CIDossierSplitting implements F
 
     public void setAction(java.lang.String newAction) {
         action = newAction;
+    }
+
+    public boolean hasLienGedActif() throws Exception {
+        CIApplication application = (CIApplication) getSession().getApplication();
+        return application.isSplittingWantLienGed() && !JadeStringUtil.isEmpty(getIdTiersInterne());
+    }
+    public String getGedServiceName() throws Exception {
+        return ((CIApplication) getSession().getApplication()).getSplittingServiceGed();
+    }
+    public String getIdTiersInterne() {
+        return ASSURE.equals(getTypePersonne()) ? getIdTiersInterneAssure() : getIdTiersInterneConjoint();
+    }
+    public String getIdTiers() {
+        return ASSURE.equals(getTypePersonne()) ? getIdTiersAssure() : getIdTiersConjoint();
     }
 }
