a8dc6f86ce87a7d328423f0008f8f6a14d2e25f0
ebsc <benjamin.schwebel@globaz.ch>
2022-05-05 17:21:24 +0200

    feat(Mariage pour tous) Gestion des tiers. Ajout des traductions.

--- webavs-sql/src/main/sql/pyxis.sql
+++ webavs-sql/src/main/sql/pyxis.sql
@@ -2,5 +2,5 @@
 INSERT INTO schema.FWCOSP (PCOSID,PPTYGR,PCONCS,PPTYCN,PPTYCL,PPTYSA,PCOSLI,PCOSDF,PCOSDM,PCOSDP,PCOIAN,PCOIDE,PCODFI,PCOITC,PCOISE,PSPY)
 VALUES (506008,'PYMOTIFHIS',1,1,0,0,'SEXE',2,1,2,2,2,2,10500006,0,VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
 INSERT INTO schema.FWCOUP (PCOSID,PLAIDE,PCOUID,PCOLUT,PSPY) VALUES (506008,'F','SEX','Changement de sexe',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
-INSERT INTO schema.FWCOUP (PCOSID,PLAIDE,PCOUID,PCOLUT,PSPY) VALUES (506008,'D','SEX','[de]Changement de sexe',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
-INSERT INTO schema.FWCOUP (PCOSID,PLAIDE,PCOUID,PCOLUT,PSPY) VALUES (506008,'I','SEX','[it]Changement de sexe',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+INSERT INTO schema.FWCOUP (PCOSID,PLAIDE,PCOUID,PCOLUT,PSPY) VALUES (506008,'D','SEX','Änderung des Geschlechts',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
+INSERT INTO schema.FWCOUP (PCOSID,PLAIDE,PCOUID,PCOLUT,PSPY) VALUES (506008,'I','SEX','Cambio di sesso',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
