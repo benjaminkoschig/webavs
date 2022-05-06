c7f37c008f304c8132275e3336fee12299f88a36
ebko <Kleemon32>
2022-05-06 10:34:38 +0200

    feature(WEBAVS-12108) - Ajouter tiers sur période "Enfant recueilli gratuitement"
    Controle obligatoire + correction label code system
    
    Ref: S211221_008

--- webavs-web/src/main/java/globaz/hera/db/famille/SFPeriode.java
+++ webavs-web/src/main/java/globaz/hera/db/famille/SFPeriode.java
@@ -190,6 +190,16 @@ public class SFPeriode extends BEntity implements ISFPeriode {
 
             _addError(statement.getTransaction(), getSession().getLabel("ERROR_DATE_OBLIGATOIRE"));
         }
+
+        if (ISFSituationFamiliale.CS_TYPE_PERIODE_ENFANT.equals(type)) {
+            _propertyMandatory(statement.getTransaction(), idRecueillant,
+                    getSession().getLabel("VALIDATE_ENFANT_RECUILLI"));
+        }
+
+        if (ISFSituationFamiliale.CS_TYPE_PERIODE_ENFANT_CONJOINT.equals(type)) {
+            _propertyMandatory(statement.getTransaction(), idRecueillant,
+                    getSession().getLabel("VALIDATE_ENFANT_RECUILLI_CONJOINT"));
+        }
     }
 
     @Override
