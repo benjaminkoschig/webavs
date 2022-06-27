95d5411c093a79b123f5b693fb0eb64cfec09824
Jean-Philippe Joubert <jjo@globaz.ch>
2022-06-27 15:10:44 +0200

    fix(mise à jour version): nettoyage Scripts
    Mise à jour des scripts de la version
    
    ref :

--- webavs-sql/src/main/sql/apg.sql
+++ webavs-sql/src/main/sql/apg.sql
@@ -1,21 +1,3 @@
--- Mise à jour des propriétés SEODOR
-UPDATE SCHEMA.JADEPROP SET PROPVAL = 'ServicePeriodsService-2-0', PSPY = to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz' WHERE PROPNAME = 'common.seodor.webservice.name';
-UPDATE SCHEMA.JADEPROP SET PROPVAL = 'http://www.zas.admin.ch/seodor/ws/service-periods/2', PSPY = to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz' WHERE PROPNAME = 'common.seodor.webservice.namespace';
-
--- FIELDNAME_NOMBREJOURSSUPP = "VHNNJU";
-ALTER TABLE SCHEMA.APPRESP ADD COLUMN VHNNJU DECIMAL(9);
-
--- FIELDNAME_NOMBREJOURSCONGES = "VHNNJC";
-ALTER TABLE SCHEMA.APPRESP ADD COLUMN VHNNJC DECIMAL(9);
-
--- FIELDNAME_DATEFIN_SAISIE = "VHDFIS";
-ALTER TABLE SCHEMA.APPRESP ADD COLUMN VHDFIS DECIMAL(8);
-
--- reorg table SCHEMA.APPRESP;
-call sysproc.admin_cmd('REORG TABLE APPRESP');
-
--- FIELDNAME_DATEFIN_CALCULEE = "VADDFC";
-ALTER TABLE SCHEMA.APDROITPATERNITE ADD COLUMN VADDFC DECIMAL(8);
-
--- reorg table SCHEMA.APDROITPATERNITE;
-call sysproc.admin_cmd('REORG TABLE APDROITPATERNITE');
+---------------------------------------------------------------
+-----   APG.SQL
+---------------------------------------------------------------
--- webavs-sql/src/main/sql/pavo.sql
+++ webavs-sql/src/main/sql/pavo.sql
@@ -1,2 +1,3 @@
-insert into SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY) VALUES ('pavo.splitting.wantLienGed', 'false', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
-insert into SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY) VALUES ('pavo.splitting.serviceGed', '', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+---------------------------------------------------------------
+-----   PAVO.SQL
+---------------------------------------------------------------
