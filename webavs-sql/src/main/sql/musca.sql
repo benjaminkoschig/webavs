5b8bc1a4cd523daf4b612b0b38ba6334dbf51ff0
Jean-Philippe Joubert <jjo@globaz.ch>
2022-06-27 15:17:30 +0200

    fix(mise à jour version): nettoyage Scripts
    Mise à jour des scripts de la version
    
    ref :

--- webavs-sql/src/main/sql/musca.sql
+++ webavs-sql/src/main/sql/musca.sql
@@ -1,4 +1,3 @@
--- FIELD_EBILL_PRINTED = "EBILLPRINTED";
-ALTER TABLE SCHEMA.FAENTFP ADD COLUMN EBILLPRINTED VARCHAR(1);
-reorg table SCHEMA.FAENTFP;
--- call sysproc.admin_cmd('REORG TABLE FAENTFP);
+---------------------------------------------------------------
+-----   MUSCA.SQL
+---------------------------------------------------------------
