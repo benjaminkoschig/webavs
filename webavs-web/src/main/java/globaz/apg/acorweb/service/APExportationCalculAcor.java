f61e507d284db47f485ecb1ab88588acb13cbf72
ebko <Kleemon32>
2022-07-07 15:17:08 +0200

    feature(S220119_005) ENSAVS - ACOR APG
    Mapping des données Maternité
    
    Ref:WEBAVS-12291

--- webavs-web/src/main/java/globaz/apg/acorweb/service/APExportationCalculAcor.java
+++ webavs-web/src/main/java/globaz/apg/acorweb/service/APExportationCalculAcor.java
@@ -5,6 +5,7 @@ import acor.ch.admin.zas.xmlns.acor_rentes_in_host._0.DemandeType;
 import acor.ch.admin.zas.xmlns.acor_rentes_in_host._0.InHostType;
 import acor.ch.admin.zas.xmlns.acor_rentes_in_host._0.TypeDemandeEnum;
 import ch.admin.zas.xmlns.in_apg._0.AllocationPerteGainAPG;
+import ch.admin.zas.xmlns.in_apg._0.BasesCalculAMat;
 import ch.admin.zas.xmlns.in_apg._0.BasesCalculAPG;
 import ch.admin.zas.xmlns.in_apg._0.GarantieIJ;
 import ch.globaz.common.exceptions.CommonTechnicalException;
@@ -86,11 +87,13 @@ public class APExportationCalculAcor {
                                              final BSession session) {
 
             AllocationPerteGainAPG allocationPerteGainAPG = new AllocationPerteGainAPG();
+            List<APSituationProfessionnelle> situationsProfessionnelles = loadSituationsProfessionnelles();
             if (droit instanceof APDroitAPG) {
-                List<APSituationProfessionnelle> situationsProfessionnelles = loadSituationsProfessionnelles();
                 allocationPerteGainAPG.setBasesCalculAPG(mapToBaseCalculAPG((APDroitAPG) droit, situationsProfessionnelles, session));
-                allocationPerteGainAPG.getRevenu().addAll(new APRevenuMapper(situationsProfessionnelles, droit).map(session));
+            } else if(droit instanceof APDroitMaternite) {
+                allocationPerteGainAPG.setBasesCalculAMat(mapToBaseCalculAPG((APDroitMaternite) droit, situationsProfessionnelles, session));
             }
+            allocationPerteGainAPG.getRevenu().addAll(new APRevenuMapper(situationsProfessionnelles, droit).map(session));
             assureType.setAllocationPerteGain(allocationPerteGainAPG);
         return assureType;
     }
@@ -141,6 +144,56 @@ public class APExportationCalculAcor {
         for(APBaseCalcul apBase : apBases) {
             basesCalcul.getPeriode().add(new APPeriodeMapper(apBase, situationsProfessionnelles).map());
         }
+
+        return basesCalcul;
+    }
+
+    private BasesCalculAMat mapToBaseCalculAPG(final APDroitMaternite droit, List<APSituationProfessionnelle> situationsProfessionnelles, final BSession session) {
+        BasesCalculAMat basesCalcul = new BasesCalculAMat();
+
+        basesCalcul.setGenreCarte(1);
+//        basesCalcul.setCantonImpot();
+//        basesCalcul.setTauxImpot();
+//        basesCalcul.setAFac();
+//        basesCalcul.setExemptionCotisation();
+        if(!JadeStringUtil.isBlankOrZero(droit.getDroitAcquis())) {
+            GarantieIJ garantie = new GarantieIJ();
+            garantie.setMontant(Double.valueOf(droit.getDroitAcquis()));
+            garantie.setSource(Integer.valueOf(session.getCode(droit.getCsProvenanceDroitAcquis())));
+//  TODO            garantie.setNumeroReference(); --> obligatoire !
+            basesCalcul.setGarantieIJ(garantie);
+        }
+        try {
+            basesCalcul.setLimiteTransfert(Double.valueOf(session.getApplication().getProperty(
+                    APApplication.PROPERTY_MONTANT_MINIMUM_PAYE_ASSURE)));
+        } catch (Exception e) {
+            LOG.error("Impossible de récupérer la propriété "+APApplication.PROPERTY_MONTANT_MINIMUM_PAYE_ASSURE, e);
+            throw new CommonTechnicalException("Impossible de récupérer la propriété "+APApplication.PROPERTY_MONTANT_MINIMUM_PAYE_ASSURE , e);
+        }
+
+//        basesCalcul.setGenreService(Integer.valueOf(session.getCode(droit.getGenreService())));
+//
+//        if(!JadeStringUtil.isEmpty(droit.getNoCompte())) {
+//            basesCalcul.setNumeroReference(Integer.valueOf(droit.getNoCompte()));
+//        }
+//        if(!JadeStringUtil.isEmpty(droit.getNoControlePers())) {
+//            basesCalcul.setNumeroControle(Integer.valueOf(droit.getNoControlePers()));
+//        }
+//
+//        try {
+//            APSituationFamilialeAPG situationFamilialeAPG = droit.loadSituationFamilliale();
+//            basesCalcul.setFraisGarde(Double.valueOf(situationFamilialeAPG.getFraisGarde()));
+//        } catch (Exception e) {
+//            LOG.error("Impossible de récupérer la situation familliale.", e);
+//            throw new CommonTechnicalException("Impossible de récupérer la situation familliale" , e);
+//        }
+//
+//        ;
+//
+//        List<APBaseCalcul> apBases = loadBasesCalcul();
+//        for(APBaseCalcul apBase : apBases) {
+//            basesCalcul.getPeriode().add(new APPeriodeMapper(apBase, situationsProfessionnelles).map());
+//        }
 
         return basesCalcul;
     }
