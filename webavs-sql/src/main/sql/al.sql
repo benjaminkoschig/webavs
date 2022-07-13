a1011dcdb519d5c472efd04c475e626cc7af18ef
DGA <dga@globaz.ch>
2022-07-13 16:38:07 +0200

    feature(Impot source) : CCVD Activation Impot Source
    Ajout des codes systeme manquant (liste impot source AF) A valider si executé pour toutes les caisses
    
    ref: WEBAVS-12421, S220616_003

--- webavs-sql/src/main/sql/al.sql
+++ webavs-sql/src/main/sql/al.sql
@@ -2,8 +2,8 @@
 -----   AL.SQL
 ---------------------------------------------------------------
 
--- CCVD Activation impot source
-INSERT INTO SCHEMA.ALPARAM (PPAID, PSPY, PPACDI, PPARVA, PPRADE, PPADDE, PPARPD, PCOSID, CSTYUN, PPRAVN, PPARAP, PPARIA, PPARPF, PCOITC) VALUES
+-- CCVD Activation impot source : todo : mettre dans un SQL spécifique
+/* INSERT INTO SCHEMA.ALPARAM (PPAID, PSPY, PPACDI, PPARVA, PPRADE, PPADDE, PPARPD, PCOSID, CSTYUN, PPRAVN, PPARAP, PPARIA, PPARPF, PCOITC) VALUES
        (92, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', 'rubrique.standard.is', '', 'Allocations LFA aux petits paysans impôt source', '19000101', '0.000000', 0, 0, '0.000000', 'WEBAF', 0, '0.000000', 0),
        (93, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', 'rubrique.caisse.salaire.h.is', '', 'Allocations familiales Siège hors canton impôt source', '19000101', '0.000000', 0, 0, '0.000000', 'WEBAF', 0, '0.000000', 0),
        (94, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', 'rubrique.caisse.salaire.ra.is', '', 'Allocations familiales Instit enfance&jeunesse impôt source', '19000101', '0.000000', 0, 0, '0.000000', 'WEBAF', 0, '0.000000', 0),
@@ -20,3 +20,36 @@ INSERT INTO SCHEMA.ALPARAM (PPAID, PSPY, PPACDI, PPARVA, PPRADE, PPADDE, PPARPD,
        (105, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', 'rubrique.standard.non.actif.is', '', 'Allocations familiales aux PSA impôt source', '19000101', '0.000000', 0, 0, '0.000000', 'WEBAF', 0, '0.000000', 0),
        (106, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', 'rubrique.standard.salarie.is', '', 'Allocations familiales Empl. Personnel non agricole impôt source', '19000101', '0.000000', 0, 0, '0.000000', 'WEBAF', 0, '0.000000', 0),
        (107, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', 'rubrique.standard.travailleur.agricole.is', '', 'Allocations LFA aux salariés agricoles impôt source', '19000101', '0.000000', 0, 0, '0.000000', 'WEBAF', 0, '0.000000', 0);
+*/
+-- CCVD Activation impot source : ajout des codes système liste
+INSERT INTO SCHEMA.FWCOSP
+(PCOSID, PPTYGR, PCONCS, PPTYCN, PPTYCL, PPTYSA, PCOSLI, PCOSDF, PCOSDM, PCOSDP, PCOIAN, PCOIDE, PCODFI, PCOITC, PCOISE, PSPY)
+VALUES (68025001, 'PTLISTAF', 1, 1, 0, 0, 'Détail-par-CAF-et-travailleur', 2, 1, 2, 2, 2, 2, 67000025, 0, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOUP
+(PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY)
+VALUES (68025001, 'D', 'DCAFT', '?Détail par CAF et travailleur', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOUP
+(PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY)
+VALUES (68025001, 'F', 'DCAFT', 'Détail par CAF et travailleur', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOUP
+(PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY)
+VALUES (68025001, 'I', 'DCAFT', '?Détail par CAF et travailleur', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOSP
+(PCOSID, PPTYGR, PCONCS, PPTYCN, PPTYCL, PPTYSA, PCOSLI, PCOSDF, PCOSDM, PCOSDP, PCOIAN, PCOIDE, PCODFI, PCOITC, PCOISE, PSPY)
+VALUES (68025002, 'PTLISTAF', 1, 1, 0, 0, 'Retenue-et-commission-par-CAF', 2, 1, 2, 2, 2, 2, 67000025, 0, to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOUP
+(PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY)
+VALUES (68025002, 'D', 'RCCAF', '?Retenue et commission par CAF', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOUP
+(PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY)
+VALUES (68025002, 'F', 'RCCAF', 'Retenue et commission par CAF', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+
+INSERT INTO SCHEMA.FWCOUP
+(PCOSID, PLAIDE, PCOUID, PCOLUT, PSPY)
+VALUES (68025002, 'I', 'RCCAF', '?Retenue et commission par CAF', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
