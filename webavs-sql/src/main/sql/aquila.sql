3879dcce81a2e12995b11eba9633d8b98a5b8be7
ESVE <esve@globaz.ch>
2022-07-18 13:49:38 +0200

    feat(eBill): eBill Factures Hors Plan Facturation + propriétés d'activation séparées l'une de l'autre
    Ref : S220325_002, WEBAVS-12411

--- webavs-sql/src/main/sql/aquila.sql
+++ webavs-sql/src/main/sql/aquila.sql
@@ -4,6 +4,5 @@
 
 -- eBill propriétés systèmes d'activation par module
 INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('aquila.eBill.activer','false',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz', VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('musca.eBill.activer','false',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz', VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
 reorg table SCHEMA.JADEPROP;
 -- call sysproc.admin_cmd('REORG TABLE JADEPROP');
