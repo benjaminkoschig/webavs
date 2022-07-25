56bf05647e5451790105d0b914c3d68e968ed13d
Jean-Philippe Joubert <jjo@globaz.ch>
2022-07-25 12:06:55 +0200

    upgrade(sql): Clean des scripts sql
    Clean des scripts sql pour la version 1.28.0-1
    
    ref :

--- webavs-sql/src/main/sql/hercule.sql
+++ webavs-sql/src/main/sql/hercule.sql
@@ -1,5 +1,3 @@
 ---------------------------------------------------------------
 -----   HERCULE.SQL
 ---------------------------------------------------------------
-
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('hercule.codeNoga','false',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
--- webavs-sql/src/main/sql/musca.sql
+++ webavs-sql/src/main/sql/musca.sql
@@ -1,8 +1,3 @@
 ---------------------------------------------------------------
 -----   MUSCA.SQL
 ---------------------------------------------------------------
-
--- FIELD_EBILL_PRINTED = "EBILLPRINTED";
-ALTER TABLE SCHEMA.FAENTFP ADD COLUMN EBILLPRINTED VARCHAR(1);
-reorg table SCHEMA.FAENTFP;
--- call sysproc.admin_cmd('REORG TABLE FAENTFP);
