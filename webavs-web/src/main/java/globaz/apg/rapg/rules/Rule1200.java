c717ae9f0cf6db530210cd9ac407f5304e7f7826
DGA <dga@globaz.ch>
2022-07-27 13:06:34 +0200

    feature(IS Prestation) : Breakrule pour tous les modules
    Generalisation de la règle pour APG, Paternité, Maternité, Pandémie et Proche Aidant
    
    ref: WEBAVS-12589, S220407_004

--- webavs-web/src/main/java/globaz/apg/rapg/rules/Rule1200.java
+++ webavs-web/src/main/java/globaz/apg/rapg/rules/Rule1200.java
@@ -2,11 +2,9 @@ package globaz.apg.rapg.rules;
 
 import ch.globaz.common.properties.PropertiesException;
 import ch.globaz.pyxis.domaine.constantes.CodeIsoPays;
-import globaz.apg.db.droits.APDroitMaternite;
-import globaz.apg.db.droits.APDroitMaterniteJointTiers;
+import globaz.apg.db.droits.APDroitLAPG;
 import globaz.apg.db.droits.APSituationProfessionnelle;
 import globaz.apg.db.droits.APSituationProfessionnelleManager;
-import globaz.apg.enums.APGenreServiceAPG;
 import globaz.apg.exceptions.APRuleExecutionException;
 import globaz.apg.exceptions.APWebserviceException;
 import globaz.apg.pojo.APChampsAnnonce;
@@ -16,8 +14,8 @@ import globaz.pyxis.db.adressecourrier.TIPays;
 import globaz.pyxis.db.tiers.TITiers;
 
 /**
- * Règle de validation des plausibilités Imposition à la source Maternité selon spécificités définies dans S220407_004.
- * Ne laisse pas passer un calcul sur un droit maternité imposé à la source payé à l'assuré dont la nationalité et
+ * Règle de validation des plausibilités Imposition à la source selon spécificités définies dans S220407_004.
+ * Ne laisse pas passer un calcul sur un droit non imposé à la source payé à l'assuré dont la nationalité et
  * le pays du droit ne sont pas la Suisse.
  */
 public class Rule1200 extends Rule{
@@ -28,36 +26,29 @@ public class Rule1200 extends Rule{
 
     @Override
     public boolean check(APChampsAnnonce champsAnnonce) throws APRuleExecutionException, IllegalArgumentException, APWebserviceException, PropertiesException {
-        if(APGenreServiceAPG.Maternite.getCodePourAnnonce().equals(champsAnnonce.getServiceType())){
-            try {
-                APDroitMaternite droit = new APDroitMaternite();
-                droit.setSession(getSession());
-                droit.setIdDroit(champsAnnonce.getIdDroit());
-                droit.retrieve();
-                if(Boolean.FALSE.equals(droit.getIsSoumisImpotSource())){
-                    APDroitMaterniteJointTiers droitJoinTiers = new APDroitMaterniteJointTiers();
-                    droitJoinTiers.setSession(getSession());
-                    droitJoinTiers.setIdDroit(champsAnnonce.getIdDroit());
-                    droitJoinTiers.retrieve();
+        try {
+            APDroitLAPG droit = new APDroitLAPG();
+            droit.setSession(getSession());
+            droit.setIdDroit(champsAnnonce.getIdDroit());
+            droit.retrieve();
+            if(Boolean.FALSE.equals(droit.getIsSoumisImpotSource())){
+                TITiers tiers = new TITiers();
+                tiers.setSession(getSession());
+                tiers.setIdTiers(droit.loadDemande().getIdTiers());
+                tiers.retrieve();
 
-                    TITiers tiers = new TITiers();
-                    tiers.setSession(getSession());
-                    tiers.setIdTiers(droitJoinTiers.getIdTiers());
-                    tiers.retrieve();
-
-                    TIPays paysNationalite = new TIPays();
-                    paysNationalite.setSession(getSession());
-                    paysNationalite.setIdPays(tiers.getIdPays());
-                    paysNationalite.retrieve();
-                    if(!CodeIsoPays.SUISSE.getCodeIso().equals(paysNationalite.getCodeIso())
-                            && !IConstantes.ID_PAYS_SUISSE.equals(droit.getPays())
-                            && hasEmployeAsEmployeurAdressePaiement(champsAnnonce.getIdDroit())){
-                        return false;
-                    }
+                TIPays paysNationalite = new TIPays();
+                paysNationalite.setSession(getSession());
+                paysNationalite.setIdPays(tiers.getIdPays());
+                paysNationalite.retrieve();
+                if(!CodeIsoPays.SUISSE.getCodeIso().equals(paysNationalite.getCodeIso())
+                        && !IConstantes.ID_PAYS_SUISSE.equals(droit.getPays())
+                        && hasEmployeAsEmployeurAdressePaiement(champsAnnonce.getIdDroit())){
+                    return false;
                 }
-            } catch (Exception e) {
-                throwRuleExecutionException(e);
             }
+        } catch (Exception e) {
+            throwRuleExecutionException(e);
         }
         return true;
     }
