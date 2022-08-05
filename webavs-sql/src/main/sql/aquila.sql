bf0207ea87de43307eaeb87790959881c589dbfd
Jean-Philippe Joubert <jjo@globaz.ch>
2022-08-05 09:40:58 +0200

    fix(external-xsd): mise à jour import
    mise à jour import pour eform après mis à jour version external-xsd.
    Correction sql avec reorg pour DB2 sous Datagrip
    
    ref :

--- webavs-sql/src/main/sql/aquila.sql
+++ webavs-sql/src/main/sql/aquila.sql
@@ -4,8 +4,6 @@
 
 -- eBill propriétés systèmes d'activation par module
 INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('aquila.eBill.activer','false',VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz', VARCHAR_FORMAT((current date), 'yyyymmdd') concat replace(char(current time), '.', '') concat 'globaz');
-reorg table SCHEMA.JADEPROP;
--- call sysproc.admin_cmd('REORG TABLE JADEPROP');
 
 -- eBill contentieux eBillPrinted & eBillTransactionID
 ALTER TABLE COHISTP ADD COLUMN EBILLPRINTED VARCHAR(1);
