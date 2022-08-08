3ab43f0ad63e9a193e011f702af64a8b25d1e2bb
Jean-Philippe Joubert <jjo@globaz.ch>
2022-08-08 14:57:09 +0200

    feat(ACOR APG): Exportation calcul ACOR APG
    Refactoring pour mutualiser le code d'exportation entre AMAT et APAT
    
    ref : S220119_005, WEBAVS-12301

--- webavs-web/src/main/java/globaz/apg/acorweb/mapper/APAcorBaseCalculAmatMapper.java
+++ webavs-web/src/main/java/globaz/apg/acorweb/mapper/APAcorBaseCalculAmatMapper.java
@@ -1,10 +1,17 @@
 package globaz.apg.acorweb.mapper;
 
 import acor.xsd.in.apg.BasesCalculAMat;
+import acor.xsd.in.apg.GarantieIJ;
+import ch.globaz.common.exceptions.CommonTechnicalException;
 import ch.globaz.common.util.Dates;
+import globaz.apg.application.APApplication;
+import globaz.apg.db.droits.APDroitAPG;
 import globaz.apg.db.droits.APDroitMaternite;
 import globaz.apg.db.droits.APSituationFamilialeMat;
 import globaz.globall.db.BSession;
+import globaz.jade.client.util.JadeStringUtil;
+import globaz.prestation.acor.PRACORConst;
+import globaz.prestation.acor.web.mapper.PRConverterUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,11 +27,11 @@ public class APAcorBaseCalculAmatMapper {
         BasesCalculAMat basesCalcul = new BasesCalculAMat();
 
         basesCalcul.setGenreCarte(1);
-        APAcorBaseCalculMapper.mapImpotSourceInformation(basesCalcul, droit);
+        APAcorBaseCalculMapper.setImpotSourceInformation(basesCalcul, droit);
 //        basesCalcul.setAFac();
 //        basesCalcul.setExemptionCotisation();
-        APAcorBaseCalculMapper.mapDroitAcquisInformation(session, basesCalcul, droit);
-        APAcorBaseCalculMapper.mapLimiteTransferInformation(session, basesCalcul);
+        APAcorBaseCalculMapper.setDroitAcquisInformation(session, basesCalcul, droit);
+        APAcorBaseCalculMapper.setLimiteTransferInformation(session, basesCalcul);
 
         basesCalcul.setDateDebutConge(Dates.toXMLGregorianCalendar(Dates.toDate(droit.getDateDebutDroit())));
         basesCalcul.setDateRepriseActivite(Dates.toXMLGregorianCalendar(Dates.toDate(droit.getDateRepriseActiv())));
