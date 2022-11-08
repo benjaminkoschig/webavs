a8da7081b20cae609f0eb412545ca505365e8748
ebko <Kleemon32>
2022-11-08 14:42:07 +0100

    feature(S220322_006) ORION Evolution 2 : Contrôles saisie automatique
    Renommage propriétés : filtre sur les personnalités juridiques et motifs de fin
    
    ref:WEBAVS-13542

--- webavs-sql/src/main/sql/orion.sql
+++ webavs-sql/src/main/sql/orion.sql
@@ -3,6 +3,6 @@
 ---------------------------------------------------------------
 
 -- liste des id faillite dans affiliations à filter pour le process d'import fichier pucs
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('orion.batch.import.idfaillite','', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('orion.import.motifdefin','', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
 -- liste des id personnalite juridique dans affiliations à filter pour le process d'import fichier pucs
-INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('orion.batch.import.idimmeuble','', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
+INSERT INTO SCHEMA.JADEPROP (PROPNAME,PROPVAL,CSPY,PSPY) VALUES ('orion.import.personnalitejuridique','', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
