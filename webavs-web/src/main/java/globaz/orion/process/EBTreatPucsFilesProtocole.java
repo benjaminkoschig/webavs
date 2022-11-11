8b476ac0c6bfa8f0260120627a4deb3f6b228faf
ebko <Kleemon32>
2022-11-11 11:39:36 +0100

    feature(S220322_006) ORION Evolution 2 : Contrôles saisie automatique
    Renommage propriété : filtre supplémentaire sur les personnalités juridiques
    
    ref:WEBAVS-13542

--- webavs-web/src/main/java/globaz/orion/process/EBTreatPucsFilesProtocole.java
+++ webavs-web/src/main/java/globaz/orion/process/EBTreatPucsFilesProtocole.java
@@ -1,6 +1,5 @@
 package globaz.orion.process;
 
-import ch.globaz.common.LabelCommonProvider;
 import ch.globaz.common.domaine.Date;
 import ch.globaz.common.listoutput.SimpleOutputListBuilderJade;
 import ch.globaz.orion.business.models.pucs.PucsFileMerge;
@@ -58,11 +57,11 @@ public class EBTreatPucsFilesProtocole {
                         + JadeI18n.getInstance().getMessage(session.getIdLangueISO(),
                         jadeBusinessMessage.getMessageId()) + "\n";
             }
-            message += LabelCommonProvider.getLabel("PROCESS_ERROR", session.getIdLangueISO()) + ": " + msg;
+            message += session.getLabel("PROCESS_ERROR") + ": " + msg;
         } else {
             message += session.getErrors().toString() + process.getTransaction().getErrors().toString();
             if (e != null) {
-                message += LabelCommonProvider.getLabel("PROCESS_ERROR", session.getIdLangueISO()) + ": " + e.getMessage();
+                message += session.getLabel("PROCESS_ERROR") + ": " + e.getMessage();
             }
         }
         if (e != null) {
@@ -79,7 +78,7 @@ public class EBTreatPucsFilesProtocole {
 
     public void sendProtocole(String emailAdress, BProcess process) throws Exception {
         String file = buildXlsProtocole().getAbsolutePath();
-        String titre = LabelCommonProvider.getLabel(hasError ? "PROCESS_ERROR" : "PROCESS_SUCCES", session.getIdLangueISO());
+        String titre = session.getLabel(hasError ? "PROCESS_ERROR" : "PROCESS_SUCCES");
         String body = titre + " : ";
         body += session.getLabel("BATCH_PROTOCOLE_BODY");
 
