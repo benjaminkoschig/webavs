c895108365ba44de216d2957bef483db043454c6
ebsc <benjamin.schwebel@globaz.ch>
2022-12-13 16:44:45 +0100

    feat(version) Mise à jour des pom en 1.29.2-SNAPSHOT et nettoyage des scripts.

--- webavs-sql/src/main/sql/corvus.sql
+++ webavs-sql/src/main/sql/corvus.sql
@@ -1,7 +1,3 @@
 ---------------------------------------------------------------
 -----   CORVUS.SQL
 ---------------------------------------------------------------
-
--- Ajout de la propriété d'affichage d'acor v3
-INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY)
-VALUES ('corvus.acor.utiliser.version.poste.utilisateur', 'false', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
--- webavs-sql/src/main/sql/ij.sql
+++ webavs-sql/src/main/sql/ij.sql
@@ -1,7 +1,3 @@
 ---------------------------------------------------------------
 -----   IJ.SQL
 ---------------------------------------------------------------
-
--- Ajout de la propriété d'affichage d'acor v3
-INSERT INTO SCHEMA.JADEPROP (PROPNAME, PROPVAL, CSPY, PSPY)
-VALUES ('ij.acor.utiliser.version.poste.utilisateur', 'false', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz', to_char(current timestamp, 'YYYYMMDDHH24MISS') concat 'globaz');
