641a00aa93985d4bf2a23def2df9b407afa59fef
Jean-Philippe Joubert <jjo@globaz.ch>
2023-01-26 12:09:41 +0100

    Test

--- webavs-web/src/main/java/globaz/apg/acorweb/service/APImportationCalculAcor.java
+++ webavs-web/src/main/java/globaz/apg/acorweb/service/APImportationCalculAcor.java
@@ -44,7 +44,7 @@ public class APImportationCalculAcor {
     }
 
     public void importCalculAcor(String idDroit) throws Exception {
-        LOG.info("Importation des données calculées.");
+        LOG.info("Importation des données calculées. Test");
         BSession session = entityService.getSession();
         BTransaction transaction = null;
         try{
